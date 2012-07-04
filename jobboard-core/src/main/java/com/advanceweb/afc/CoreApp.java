15fb84e93df57b71b14950dc06f0187dd5b58df1
sukesh <sukesh@localhost>
2012-07-04 05:43:57 +0000

    Restructuring the project folders to suite the standard svn configuration

---
+++ jobboard-core/src/main/java/com/advanceweb/afc/CoreApp.java
@@ -0,0 +1,16 @@
+package com.advanceweb.afc;
+
+import com.advanceweb.afc.common.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class CoreApp 
+{
+    public static void main( String[] args )
+    {
+    	App app = new App();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jobboard/src/main/java/com/advanceweb/afc/webapp/WebApp.java
@@ -0,0 +1,7 @@
+package com.advanceweb.afc.webapp;
+
+import com.advanceweb.afc.common.App;
+
+public class WebApp {
+	App app = new App();
+}
