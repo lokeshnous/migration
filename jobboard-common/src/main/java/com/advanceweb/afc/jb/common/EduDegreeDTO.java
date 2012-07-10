b747bbcd9cc4309f58a1d7e65130af78365505b5
sasibhushanam <sasibhushanam@localhost>
2012-07-10 08:00:13 +0000

    Create resume functionality

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/EduDegreeDTO.java
@@ -0,0 +1,36 @@
+package com.advanceweb.afc.jb.common;
+
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+public class EduDegreeDTO {
+	private int degreeEduId;
+	private String name;
+	private String description;
+	
+	public int getDegreeEduId() {
+		return degreeEduId;
+	}
+	public void setDegreeEduId(int degreeEduId) {
+		this.degreeEduId = degreeEduId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
