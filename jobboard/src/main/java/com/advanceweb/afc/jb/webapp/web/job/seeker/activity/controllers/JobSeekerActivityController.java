dd56cd343cd223bd1bfe5071926211f065433aef
sasibhushanam <sasibhushanam@localhost>
2012-07-10 08:58:13 +0000

    Nishanth said this file to commit.

--- jobboard/src/main/java/com/advanceweb/afc/jb/webapp/web/job/seeker/activity/controllers/JobSeekerActivityController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/webapp/web/job/seeker/activity/controllers/JobSeekerActivityController.java
@@ -1,42 +1,18 @@
 package com.advanceweb.afc.jb.webapp.web.job.seeker.activity.controllers;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.advanceweb.afc.jb.common.AppliedJobDTO;
-import com.advanceweb.afc.jb.common.CountryDTO;
 import com.advanceweb.afc.jb.common.SavedJobDTO;
-import com.advanceweb.afc.jb.dto.CreateResumeDTO;
-import com.advanceweb.afc.jb.registration.ProfileRegistration;
-import com.advanceweb.afc.jb.resume.CreateResumeService;
-import com.advanceweb.afc.jb.webapp.web.forms.registration.JobSeekerRegistrationForm;
-import com.advanceweb.afc.jb.webapp.web.forms.resume.CreateResume;
-import com.advanceweb.afc.jb.webapp.web.helper.ReadDocFile;
-import com.advanceweb.afc.jb.jobseeker.activity.*;
+import com.advanceweb.afc.jb.jobseeker.activity.JobSeekerActivity;
 
 /**
  * This controller belongs to all jobseekers activity
