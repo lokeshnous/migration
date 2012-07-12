5e77ce603d15002a0e2062412d9ba1929e8c8097
reeteshrn <reeteshrn@localhost>
2012-07-12 14:19:35 +0000

    Created DTO for SOLR job search

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/JobSearchDTO.java
@@ -0,0 +1,46 @@
+package com.advanceweb.afc.jb.common;
+
+import java.io.Serializable;
+import java.util.Date;
+import org.apache.solr.client.solrj.beans.Field;
+
+public class JobSearchDTO implements Serializable{
+	
+	private static final long serialVersionUID = -2200423309708244707L;
+	@Field
+	private String company;
+	@Field
+	private String jobtitle;
+	@Field
+	private Date posted_dt;
+	@Field
+	private String city;
+	
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public String getJobtitle() {
+		return jobtitle;
+	}
+	public Date getPosted_dt() {
+		return posted_dt;
+	}
+	public String getCity() {
+		return city;
+	}
+	
+	/*@Field
+	private String state;
+	@Field
+	private String job_id;
+	@Field
+	private String job_number;*/
+	
+	
+	
+
+}
