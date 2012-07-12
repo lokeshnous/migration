10a33fa27e5682b178c5fe2d65a6b62cc1ce13f7
sasibhushanam <sasibhushanam@localhost>
2012-07-12 11:15:39 +0000

    Modified for user subscriptions

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/data/entities/MerUserAlerts.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/data/entities/MerUserAlerts.java
@@ -2,6 +2,7 @@ package com.advanceweb.afc.jb.data.entities;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
 import java.util.Date;
 
 
@@ -13,82 +14,65 @@ import java.util.Date;
 @Table(name="mer_user_alerts")
 public class MerUserAlerts implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="alert_id")
 	private int alertId;
 
-	@Column(name="alert_status")
-	private String alertStatus;
-
     @Temporal( TemporalType.TIMESTAMP)
+    @Column(name="created_date")
 	private Date created_date;
 
-	private String email_id;
-
-	private String mobile_no;
-
-    @Temporal( TemporalType.TIMESTAMP)
-	private Date remove_date;
-
+    @Column(name="alert_value")
+	private String alertvalue;
+    
+    @Column(name="user_id")
 	private int userid;
-
-    public MerUserAlerts() {
-    }
+    
+    @Column(name="lookup_id")
+	private int lookupid;
+    
 
 	public int getAlertId() {
-		return this.alertId;
+		return alertId;
 	}
 
 	public void setAlertId(int alertId) {
 		this.alertId = alertId;
 	}
 
-	public String getAlertStatus() {
-		return this.alertStatus;
-	}
-
-	public void setAlertStatus(String alertStatus) {
-		this.alertStatus = alertStatus;
-	}
-
 	public Date getCreated_date() {
-		return this.created_date;
+		return created_date;
 	}
 
 	public void setCreated_date(Date created_date) {
 		this.created_date = created_date;
 	}
 
-	public String getEmail_id() {
-		return this.email_id;
+	public String getAlertvalue() {
+		return alertvalue;
 	}
 
-	public void setEmail_id(String email_id) {
-		this.email_id = email_id;
-	}
-
-	public String getMobile_no() {
-		return this.mobile_no;
-	}
-
-	public void setMobile_no(String mobile_no) {
-		this.mobile_no = mobile_no;
-	}
-
-	public Date getRemove_date() {
-		return this.remove_date;
-	}
-
-	public void setRemove_date(Date remove_date) {
-		this.remove_date = remove_date;
+	public void setAlertvalue(String alertvalue) {
+		this.alertvalue = alertvalue;
 	}
 
 	public int getUserid() {
-		return this.userid;
+		return userid;
 	}
 
 	public void setUserid(int userid) {
 		this.userid = userid;
 	}
 
+	public int getLookupid() {
+		return lookupid;
+	}
+
+	public void setLookupid(int lookupid) {
+		this.lookupid = lookupid;
+	}
+    
+    
 }
