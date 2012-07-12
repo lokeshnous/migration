cf61d9ffc3de2e732ec2f9a71ff2791458974675
sasibhushanam <sasibhushanam@localhost>
2012-07-12 09:02:19 +0000

    Adding job alerts, subscriptions and magazines

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/JobAlertsDTO.java
@@ -0,0 +1,26 @@
+package com.advanceweb.afc.jb.common;
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+public class JobAlertsDTO {
+	private String alertId;
+	private String alertName;
+	
+	
+	public String getAlertId() {
+		return alertId;
+	}
+	public void setAlertId(String alertId) {
+		this.alertId = alertId;
+	}
+	public String getAlertName() {
+		return alertName;
+	}
+	public void setAlertName(String alertName) {
+		this.alertName = alertName;
+	}		
+}
---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/MagazinesDTO.java
@@ -0,0 +1,30 @@
+package com.advanceweb.afc.jb.common;
+
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+
+public class MagazinesDTO {
+	private String magazineId;
+	private String magazineName;
+	
+	
+	public String getMagazineId() {
+		return magazineId;
+	}
+	public void setMagazineId(String magazineId) {
+		this.magazineId = magazineId;
+	}
+	public String getMagazineName() {
+		return magazineName;
+	}
+	public void setMagazineName(String magazineName) {
+		this.magazineName = magazineName;
+	}
+	
+	
+}
