4c68d899426b923e13bf603269e01d38c159cba0
reeteshrn <reeteshrn@localhost>
2012-07-13 13:57:09 +0000

    Created Helper class to read values from Solr properties file

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/webapp/web/helper/ReadSolrServerDetails.java
@@ -0,0 +1,33 @@
+package com.advanceweb.afc.jb.webapp.web.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ReadSolrServerDetails {
+	
+	
+	
+	public Map<String, String> getServerDetails(Properties solrConfiguration){
+		Map<String, String> serverDetailsMap = new HashMap<String, String>();
+		//String serverUrl = ;
+		System.out.println("@@@@@@@@@@@@@@@@@serverUrl===="+solrConfiguration.getProperty("url"));
+		serverDetailsMap.put("serverUrl",solrConfiguration.getProperty("url") );
+		serverDetailsMap.put("sotimeout", solrConfiguration.getProperty("sotimeout"));
+		serverDetailsMap.put("connectiontimeout", solrConfiguration.getProperty("connectiontimeout"));
+		serverDetailsMap.put("maxconnectionperhost", solrConfiguration.getProperty("maxconnectionperhost"));
+		serverDetailsMap.put("maxtotalconnection", solrConfiguration.getProperty("maxtotalconnection"));
+		serverDetailsMap.put("followredirects", solrConfiguration.getProperty("followredirects"));
+		serverDetailsMap.put("allowcompression", solrConfiguration.getProperty("allowcompression"));
+		serverDetailsMap.put("maxretries", solrConfiguration.getProperty("maxretries"));
+		
+		return serverDetailsMap;
+	}
+	
+
+}
