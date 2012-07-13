50ec0627ccf520f7f6e65e990a53a172b1cec23d
bharatiu <bharatiu@localhost>
2012-07-13 11:40:14 +0000

    Added for save this job task

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/jobsearch/JobSearchActivity.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/jobsearch/JobSearchActivity.java
@@ -1,18 +1,18 @@
 package com.advanceweb.afc.jb.jobsearch;
 
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.response.QueryResponse;
+/*import org.apache.solr.client.solrj.impl.HttpSolrServer;
+ import org.apache.solr.client.solrj.response.QueryResponse;*/
 
 import com.advanceweb.afc.jb.common.SearchResultDTO;
 import com.advanceweb.afc.jb.common.SearchedJobDTO;
 
 /**
- * <code> JobSearchActivity </code> is a Service class. 
+ * <code> JobSearchActivity </code> is a Service class.
  * 
  * @author Pramoda Patil
  * @version 1.0
  * @since 10 July 2012
- *  
+ * 
  */
 public interface JobSearchActivity {
 
@@ -23,7 +23,7 @@ public interface JobSearchActivity {
 	 * @return
 	 */
 	SearchedJobDTO viewJobDetails(long jobId);
-	
+
 	/**
 	 * apply job by job ID
 	 * 
@@ -31,30 +31,37 @@ public interface JobSearchActivity {
 	 * @return
 	 */
 	void applyJob(long jobId);
-	
+
 	/**
 	 * Gets the job search result by searchString
 	 * 
 	 * @param SearchString
 	 * @return SearchResultDTO
 	 */
-	 public SearchResultDTO getJobSearchResult(String SearchString);
-	
-	 /**
+	public SearchResultDTO getJobSearchResult(String SearchString);
+
+	/**
+	 * saves the job for logged in user
+	 * 
+	 * @param searchedJobDTO
+	 */
+	public void saveJob(SearchedJobDTO searchedJobDTO);
+
+	/**
 	 * Returns the SOLR Server object
 	 * 
-	 * @param 
+	 * @param
 	 * @return HttpSolrServer
 	 */
-	  public HttpSolrServer connectToSOLRURL();
-	  
-	  /**
+	public HttpSolrServer connectToSOLRURL();
+
+	/**
 	 * Returns the SOLR Server response object
 	 * 
 	 * @param HttpSolrServer
 	 * @return QueryResponse
 	 */
-	  public QueryResponse executeSearchQuery(HttpSolrServer server, String SearchString, String rows, String start);
-	
+	public QueryResponse executeSearchQuery(HttpSolrServer server,
+			String SearchString, String rows, String start);
 
 }
