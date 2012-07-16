d2ff352be712711d54a6027b78ddaa6299e1da36
nishantn <nishantn@localhost>
2012-07-16 12:10:32 +0000

    html page view added

--- jobboard/src/main/java/com/advanceweb/afc/jb/webapp/web/controllers/jspviewcontents/JspViewContentController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/webapp/web/controllers/jspviewcontents/JspViewContentController.java
@@ -3,59 +3,47 @@ package com.advanceweb.afc.jb.webapp.web.controllers.jspviewcontents;
  * @author nishantn
  */
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.advanceweb.afc.jb.web.utils.Constants;
 import com.advanceweb.afc.jb.web.utils.CopyUtil;
-import com.advanceweb.afc.jb.web.utils.ReadXmlFile;
-import com.advanceweb.afc.jb.webapp.web.forms.xmldata.CareersToolResource;
-import com.advanceweb.afc.jb.webapp.web.forms.xmldata.HealthCareNew;
+import com.advanceweb.afc.jb.web.utils.ReadFile;
 
 @Controller
 @RequestMapping(value="/jspContentView")
 
 public class JspViewContentController {
 
-	@RequestMapping(value = "/viewxmlContents", method = RequestMethod.GET)
-	public String getXmlContents(HttpServletRequest request,Model model) {
-		model.addAttribute("viewxml", true);
-		try {
-			List<HealthCareNew> healthlist=ReadXmlFile.xmlParser(Constants.XML_FILE_NAME_HEALTHCARE);
-			List<CareersToolResource> careerlist=ReadXmlFile.xmlParser(Constants.XML_FILE_NAME_CAREERTOOL);
 
-			model.addAttribute("healthcarenew", healthlist);
-			model.addAttribute("careerstoolresource", careerlist);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			model.addAttribute("healthcarenew", "");
-			model.addAttribute("careerstoolresource", "");
-			e.printStackTrace();
-		}
-		return "jspviewcontent";
-	}
-	
-	
-	@RequestMapping(value = "/viewcareerlinkContents", method = RequestMethod.GET)
-	public String getcareerlinkContents(HttpServletRequest request,Model model) {
-		model.addAttribute("viewcareer", true);
+	private @Value("${basedirectorypath}") String basedirectorypath;
+	private @Value("${directory}") String directory;
+	private @Value("${healthcarenewsfilename}") String healthcarenewsfilename;
+	private @Value("${careertoolfilename}") String careertoolfilename;
+
+
+	@RequestMapping(value = "/viewcareerhtmlContents", method = RequestMethod.GET)
+	public String gethtmlContents(HttpServletRequest request,Model model) {
+		model.addAttribute("viewhtml", true);
 		try {
-			List<CareersToolResource> careerlist=ReadXmlFile.xmlParser(Constants.XML_FILE_NAME_CAREERTOOL);
-			model.addAttribute("careerstoolresource", careerlist);
-			for(CareersToolResource cr:careerlist){
-				if(request.getParameter("id").equalsIgnoreCase(String.valueOf(cr.getId()))){
-					model.addAttribute("careerstoolresourcedescription", cr.getDescription());
-					model.addAttribute("careerstoolresourcename", cr.getName());
-				}
+
+			if(new File(basedirectorypath+directory+healthcarenewsfilename).exists()){
+				String htmlhealthcontent=ReadFile.htmlReader(basedirectorypath+directory+healthcarenewsfilename);
+				model.addAttribute("healthcarenew", htmlhealthcontent);
 			}
 
+			if(new File(basedirectorypath+directory+careertoolfilename).exists()){
+				String htmlcareercontent=ReadFile.htmlReader(basedirectorypath+directory+careertoolfilename);
+				model.addAttribute("careerstoolresource", htmlcareercontent);
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			model.addAttribute("healthcarenew", "");
@@ -65,15 +53,21 @@ public class JspViewContentController {
 		return "jspviewcontent";
 	}
 
-	@RequestMapping(value = "/copyxmltolocal", method = RequestMethod.GET)
-	public String copyFiles(HttpServletRequest request,Model model) {
-		List<String> li=new ArrayList<String>();
-		li.add("HealthCareNews.xml");
-		li.add("CareerToolResource.xml");
+	@RequestMapping(value = "/copyhtmltolocal", method = RequestMethod.GET)
+	public String copyHtmlFiles(HttpServletRequest request,Model model) {
+		try{
+			(new File(basedirectorypath+directory)).mkdir();
+			List<String> li=new ArrayList<String>();
+			li.add(healthcarenewsfilename);
+			li.add(careertoolfilename);
+			CopyUtil.copy(li,basedirectorypath+directory);
+			model.addAttribute("copyhtml", true);
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+			model.addAttribute("copyhtml", "");
+		}
 
-		CopyUtil.copy(li);
 		return "jspviewcontent";
 	}
-
 
 }
