6c9f01635ccd16c8ed005282e88a038ce03a5383
sasibhushanam <sasibhushanam@localhost>
2012-07-17 11:09:34 +0000

    Employer Account Setting functionality

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/EmployerAccountProfileForm.java
@@ -0,0 +1,85 @@
+package com.advanceweb.afc.jb.employer.web.controller;
+
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+public class EmployerAccountProfileForm {
+	private String contactName;
+	private String company;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private String country;
+	private String email;
+	private String phone;
+	private String profileType;
+	
+	
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getProfileType() {
+		return profileType;
+	}
+	public void setProfileType(String profileType) {
+		this.profileType = profileType;
+	}
+	
+	
+}
