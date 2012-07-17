09ba48c75e29af2f71b159779144a102937c0d20
nishantn <nishantn@localhost>
2012-07-17 13:18:05 +0000

    removed SOP

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/resume/ResumeServiceTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/resume/ResumeServiceTest.java
@@ -2,6 +2,7 @@ package com.advanceweb.afc.jb.resume;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.InetAddress;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,33 +16,28 @@ public class ResumeServiceTest extends ServiceTest {
 	@Autowired
 	private ResumeService resumeService;
 
-	
+
 	@Test
 	public void testCreateResumeUpload() {
 		try {
 			ResumeDTO createResumeDTO=new ResumeDTO();
-
 			createResumeDTO.setUserId(Integer.parseInt("1"));
 			createResumeDTO.setResumeType("Upload");
 			createResumeDTO.setResume_name("Test");
-			
-			
 			createResumeDTO.setDesired_job_title("Tilt");
-			createResumeDTO.setDesired_employment_type("1");
-			createResumeDTO.setWork_authorization_US("1");
+			createResumeDTO.setDesired_employment_type("31");
+			createResumeDTO.setWork_authorization_US("35");
 			createResumeDTO.setWilling_to_relocate("Y");
 			createResumeDTO.setResume_visibility("1");			
-
-			
-			createResumeDTO.setResumeText("Testttttttttttttttttt");
+			createResumeDTO.setResumeText("Testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt");
 			createResumeDTO.setIsPublished("1");
 
-			
-			createResumeDTO.setFileServer("10.24.64.42");
+			InetAddress ownIP=InetAddress.getLocalHost();
+
+			createResumeDTO.setFileServer(ownIP.getHostAddress());
 			createResumeDTO.setFileName("Test");
 			createResumeDTO.setFilePath("filePath");
 			assertTrue("Upload Resume", resumeService.createResumeUpload(createResumeDTO));
-			System.out.println("Resume Upload");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -55,26 +51,21 @@ public class ResumeServiceTest extends ServiceTest {
 			createResumeDTO.setUserId(Integer.parseInt("1"));
 			createResumeDTO.setResumeType("CopyPaste");
 			createResumeDTO.setResume_name("Test");
-			
-			
 			createResumeDTO.setDesired_job_title("Tilt");
-			createResumeDTO.setDesired_employment_type("1");
-			createResumeDTO.setWork_authorization_US("1");
+			createResumeDTO.setDesired_employment_type("31");
+			createResumeDTO.setWork_authorization_US("35");
 			createResumeDTO.setWilling_to_relocate("Y");
 			createResumeDTO.setResume_visibility("1");			
-
-			
-			createResumeDTO.setResumeText("Testttttttttttttttttt");
+			createResumeDTO.setResumeText("Testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt");
 			createResumeDTO.setIsPublished("1");
 			assertTrue("Copy Paste Resume", resumeService.createResumeUpload(createResumeDTO));
-			System.out.println("Resume Copy Paste");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
+
 	@Test
 	public void testRetrieveAllResumes() {
 		try {
@@ -82,7 +73,7 @@ public class ResumeServiceTest extends ServiceTest {
 			List<ResumeDTO> resumeDTOList = resumeService.retrieveAllResumes(2);
 			assertTrue("Retrieve Resume", resumeDTOList.size() >= 0);
 			for (ResumeDTO resumeDTO : resumeDTOList) {
-				System.out.println(resumeDTO);
+				//System.out.println(resumeDTO);
 			}
 
 		} catch (Exception e) {
@@ -96,7 +87,7 @@ public class ResumeServiceTest extends ServiceTest {
 
 			ResumeDTO resumeDTO = resumeService.editResume(2);
 			assertTrue("Edit Resume", resumeDTO != null);
-			System.out.println(resumeDTO);
+			//System.out.println(resumeDTO);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -107,7 +98,7 @@ public class ResumeServiceTest extends ServiceTest {
 	public void testDeleteResume() {
 		try {
 			assertTrue("Edit Resume", resumeService.deleteResume(22));
-			System.out.println("Resume deleted");
+			//System.out.println("Resume deleted");
 
 		} catch (Exception e) {
 			e.printStackTrace();
