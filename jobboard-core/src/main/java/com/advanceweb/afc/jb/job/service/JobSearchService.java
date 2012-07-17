938a851871d3ff65cc30d899ab88895f8d419919
reeteshrn <reeteshrn@localhost>
2012-07-17 10:28:34 +0000

    Created the interface for Job Search

---
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/job/service/JobSearchService.java
@@ -0,0 +1,20 @@
+package com.advanceweb.afc.jb.job.service;
+
+import java.util.Map;
+
+import com.advanceweb.afc.jb.search.engine.solr.JobSearchResultDTO;
+
+public interface JobSearchService{
+	/**
+	 * Does the Job Search
+	 * @param searchName
+	 * @param paramMap
+	 * @param rows
+	 * @param start
+	 * @return JobSearchResultDTO
+	 */
+	
+	JobSearchResultDTO jobSearch(String searchName, Map<String, String> paramMap, long rows, long start);
+	
+
+}
