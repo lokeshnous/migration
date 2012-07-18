027c59b2bdc8b40878300e180bdcbdf03ba9fcb9
reeteshrn <reeteshrn@localhost>
2012-07-18 12:27:10 +0000

    After PMD check

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchDeleagateImpl.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchDeleagateImpl.java
@@ -11,11 +11,6 @@ import java.util.Map;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import org.apache.log4j.Logger;
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.apache.solr.client.solrj.response.QueryResponse;
@@ -25,7 +20,7 @@ import org.springframework.stereotype.Service;
 @Service("jobSearchDeleagate")
 public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 
-	Logger log = Logger.getLogger("logfile");
+	//Logger log = Logger.getLogger("logfile");
 	
 	@Autowired
 	private ReadSolrServerDetails readSolrServerDetails;
@@ -44,11 +39,10 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 			final Map<String, String> paramMap, final long rows, final long start) {
 
 		//log.info("");
-		HttpSolrServer server = null;
+		//HttpSolrServer server = null;
 		QueryResponse response = null;
 		JobSearchResultDTO jobSearchResultDTO = null;
 		SolrJobSearchResultDTO solrJobSearchResultDTO = null;
-		JobSearchDTO jobSearchDTO = new JobSearchDTO();
 
 		Map<String, String> serverDetailsMap = readSolrServerDetails
 				.getServerDetails(solrConfiguration);
@@ -68,16 +62,16 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 
 			if (connection.getResponseCode() == 200) {
 				serverAccessibility = true;
-				System.out.println("Server URL " + url + " is accessible.");
+				//System.out.println("Server URL " + url + " is accessible.");
 			}
 		} catch (final MalformedURLException e) {
 			serverAccessibility = false;
-			System.out.println("e1==" + e);
-			System.out.println("Server URL " + url + " is not accessible.");
+			//System.out.println("e1==" + e);
+			//System.out.println("Server URL " + url + " is not accessible.");
 		} catch (final IOException e) {
-			System.out.println("e2==" + e);
+			//System.out.println("e2==" + e);
 			serverAccessibility = false;
-			System.out.println("Server URL " + url + " is not accessible.");
+			//System.out.println("Server URL " + url + " is not accessible.");
 		}
 
 		if (serverAccessibility) {
@@ -85,113 +79,35 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 			if ("".equalsIgnoreCase(paramMap.get("titlesearch"))
 					|| paramMap.get("titlesearch") == null) {
 
-				System.out
-				.println("Empty Search criteria. Please enter a search criteria to seach jobs.");
+				//System.out
+				//.println("Empty Search criteria. Please enter a search criteria to seach jobs.");
 				return null;
 				
 			} else {
-				server = new HttpSolrServer(serverDetailsMap.get("serverUrl")
-						.toString()
-						+ serverDetailsMap.get("solrservice").toString());
-				server.setSoTimeout(Integer.parseInt(serverDetailsMap
-						.get("sotimeout")));
-				server.setConnectionTimeout(Integer.parseInt(serverDetailsMap
-						.get("connectiontimeout")));
-				server.setDefaultMaxConnectionsPerHost(Integer
-						.parseInt(serverDetailsMap.get("maxconnectionperhost")));
-				server.setMaxTotalConnections(Integer.parseInt(serverDetailsMap
-						.get("maxtotalconnection")));
-
-				server.setFollowRedirects(Boolean.parseBoolean(serverDetailsMap
-						.get("followredirects"))); // defaults to false
-				server.setAllowCompression(Boolean
-						.parseBoolean(serverDetailsMap.get("allowcompression")));
-				server.setMaxRetries(Integer.parseInt(serverDetailsMap
-						.get("maxretries")));
-				server.setParser(new XMLResponseParser());
-
-				String searchString = paramMap.get("titlesearch");
-				if (searchString != null && searchString.length() > 0) {
-
-					SolrQuery searchquery = new SolrQuery();
-					searchquery.setQuery(searchString);
-					searchquery.setFacet(true);
-					searchquery.addFacetField(solrQueryDetails.get("city"));
-					searchquery.addFacetField(solrQueryDetails.get("company"));
-					// searchquery.addFacetField(solrQueryDetails.get("radius"));
-					searchquery
-							.addFacetField(solrQueryDetails.get("posted_dt"));
-					searchquery.addFacetField(solrQueryDetails.get("state"));
-					searchquery.add("rows", String.valueOf(rows));
-					searchquery.add("start", String.valueOf(start));
-					System.out.println("Search query===>>>" + searchquery);
-
-					try {
-						response = server.query(searchquery);
-					} catch (SolrServerException e1) {
-						e1.printStackTrace();
-					}
-				}
+				
+				response = readSolrServerDetails.getSolrResponse(serverDetailsMap, 
+						solrQueryDetails, paramMap, start, rows);
+				
 				solrJobSearchResultDTO = new SolrJobSearchResultDTO();
-				System.out.println("Number of search records===>>>"
-						+ response.getResults().getNumFound());
+				//System.out.println("Number of search records===>>>"
+						//+ response.getResults().getNumFound());
 
 				solrJobSearchResultDTO.setTotalNumSearchResult(response
 						.getResults().getNumFound());
 
 				List<JobSearchDTO> jobSearchDTOList = new ArrayList<JobSearchDTO>();
 				jobSearchDTOList = response.getBeans(JobSearchDTO.class);
-
-				for(JobSearchDTO jobSrchDTO : jobSearchDTOList){
-					
-					System.out.println("@Company===>>"
-							+ jobSrchDTO.getCompany());
-					System.out.println("@Job Title===>>"
-							+ jobSrchDTO.getJobTitle());
-					System.out.println("@City===>>" + jobSrchDTO.getCity());
-					System.out.println("@Posted Date===>>"
-							+ jobSrchDTO.getPostedDate());
-					System.out.println("@Apply Online===>>"
-							+ jobSrchDTO.getApplyOnline());
-					System.out.println("@Blind Ad===>>"
-							+ jobSearchDTO.getBlindAd());
-					System.out.println("@Facility Name===>>"
-							+ jobSearchDTO.getFacilityName());
-					System.out.println("@Email Display===>>"
-							+ jobSearchDTO.getEmailDisplay());
-					System.out.println("@Email===>>" + jobSearchDTO.getEmail());
-					System.out.println("@Is Inter===>>"
-							+ jobSearchDTO.isInternationalJob());
-					System.out.println("@Is National===>>"
-							+ jobSearchDTO.isNationalJob());
-					System.out.println("@Is Featured===>>"
-							+ jobSearchDTO.isFeatured());
-					System.out.println("@Job count===>>"
-							+ jobSearchDTO.getJobCount());
-					System.out
-							.println("@Job id===>>" + jobSearchDTO.getJobId());
-					System.out.println("@Job Number===>>"
-							+ jobSearchDTO.getJobNumber());
-					System.out.println("@Job Geo===>>"
-							+ jobSearchDTO.getJobGeo());
-					System.out.println("@Job position===>>"
-							+ jobSearchDTO.getJobPosition());
-					System.out.println("@jobGeo0LatLon===>>"
-							+ jobSearchDTO.getJobGeo0LatLon());
-					System.out.println("@jobGeo1LatLon===>>"
-							+ jobSearchDTO.getJobGeo1LatLon());
-					System.out.println("@URL Display===>>"
-							+ jobSearchDTO.getUrlDisplay());
-					
-				}
+				
+				//For displaying the results. Should be removed when UI will come.
+				readSolrServerDetails.displayResults(jobSearchDTOList);
 
 				Map<String, List<Count>> facetMap = new HashMap<String, List<Count>>();
 				List<FacetField> facetFieldList = response.getFacetFields();
 
 				for (FacetField facetField : facetFieldList) {
 					facetMap.put(facetField.getName(), facetField.getValues());
-					System.out.println("@Facet Name===>>"+ facetField.getName()+",@Facet Values(Categories)===>>>"
-							+ facetMap.get(facetField.getName()));
+					//System.out.println("@Facet Name===>>"+ facetField.getName()+",@Facet Values(Categories)===>>>"
+							//+ facetMap.get(facetField.getName()));
 				}
 
 				solrJobSearchResultDTO.setFacetMap(facetMap);
@@ -207,8 +123,8 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 			
 		
 		} else {
-			System.out
-					.println("Server url is not correct. Please check the url.");
+			//System.out
+					//.println("Server url is not correct. Please check the url.");
 			return null;
 		}
 
