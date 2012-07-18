cd166a3adc124183d198fe29c31cb5408585b173
anilm <anilm@localhost>
2012-07-18 06:53:47 +0000

    JUnit change

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/ServiceTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/ServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:HibernateSessionFactory.xml" })
+@ContextConfiguration(locations = { "classpath:resources/HibernateSessionFactory.xml" })
 public class ServiceTest extends AbstractJUnit4SpringContextTests {
 
 	@BeforeClass
