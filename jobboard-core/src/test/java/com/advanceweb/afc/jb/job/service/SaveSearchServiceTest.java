a96f448712256eb540b862ae494bd4588ff80aaf
bharatiu <bharatiu@localhost>
2012-07-18 11:02:14 +0000

    Added for the JUnit test for save this search task

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/job/service/SaveSearchServiceTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/job/service/SaveSearchServiceTest.java
@@ -2,9 +2,15 @@ package com.advanceweb.afc.jb.job.service;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
 import java.util.List;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.advanceweb.afc.jb.ServiceTest;
 import com.advanceweb.afc.jb.common.SaveSearchedJobsDTO;
 import com.advanceweb.afc.jb.data.entities.JpSaveSearch;
@@ -17,10 +23,12 @@ public class SaveSearchServiceTest extends ServiceTest {
 	@Test
 	public void testViewMySavedSearches() {
 		try {
-				int userId = 1;
-				List<SaveSearchedJobsDTO> saveSearchedJobsDTOs = saveSearchService.viewMySavedSearches(userId);
-				assertTrue("View My Saved Searches", saveSearchedJobsDTOs.size() >= 0);
-			
+			int userId = 1;
+			List<SaveSearchedJobsDTO> saveSearchedJobsDTOs = saveSearchService
+					.viewMySavedSearches(userId);
+			assertTrue("View My Saved Searches",
+					saveSearchedJobsDTOs.size() >= 0);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -29,8 +37,9 @@ public class SaveSearchServiceTest extends ServiceTest {
 	@Test
 	public void testEditSavedSearch() {
 		try {
-			int saveSearchId = 16 ; 
-			JpSaveSearch jpSaveSearch = saveSearchService.editSavedSearch(saveSearchId);
+			int saveSearchId = 16;
+			JpSaveSearch jpSaveSearch = saveSearchService
+					.editSavedSearch(saveSearchId);
 			assertNull(jpSaveSearch);
 
 		} catch (Exception e) {
@@ -38,12 +47,29 @@ public class SaveSearchServiceTest extends ServiceTest {
 		}
 	}
 
-	//@Test
+	// @Test
 	public void testDeleteSavedSearch() {
 		try {
-			
-			assertTrue("Delete a Saved Search", saveSearchService.deleteSavedSearch(17));
 
+			assertTrue("Delete a Saved Search",
+					saveSearchService.deleteSavedSearch(17));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Added for save this search
+	@Test
+	public void testSaveSearchedJobs() {
+		try {
+			SaveSearchedJobsDTO saveSearchedJobsDTO = new SaveSearchedJobsDTO();
+			saveSearchedJobsDTO.setLoginID("9");
+			saveSearchedJobsDTO
+					.setUrl("file://///nibc452/06%20JOB%20PORTAL/06%20IA/JobBoardPortalIAFinal/JobBoardPortalIAVer18/HTML/SearchResults.html?button2=Find+Jobs");
+			saveSearchedJobsDTO.setUrlName("JobName");
+			saveSearchedJobsDTO.setCreatedDate(new Date());
+			saveSearchService.saveSearchedJobs(saveSearchedJobsDTO);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
