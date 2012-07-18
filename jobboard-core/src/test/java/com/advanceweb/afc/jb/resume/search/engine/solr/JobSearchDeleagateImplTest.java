1469e1b4615ca0c553b470d0432a80e4ec4c1479
reeteshrn <reeteshrn@localhost>
2012-07-18 09:53:07 +0000

    After PMD check

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/resume/search/engine/solr/JobSearchDeleagateImplTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/resume/search/engine/solr/JobSearchDeleagateImplTest.java
@@ -5,42 +5,34 @@ import static org.junit.Assert.assertTrue;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
-import org.hibernate.transaction.JOnASTransactionManagerLookup;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.advanceweb.afc.jb.ServiceTest;
-import com.advanceweb.afc.jb.common.ResumeDTO;
-import com.advanceweb.afc.jb.resume.ResumeService;
 import com.advanceweb.afc.jb.search.engine.solr.JobSearchDTO;
 import com.advanceweb.afc.jb.search.engine.solr.JobSearchDeleagate;
-import com.advanceweb.afc.jb.search.engine.solr.JobSearchDeleagateImpl;
 import com.advanceweb.afc.jb.search.engine.solr.JobSearchResultDTO;
-import com.advanceweb.afc.jb.search.engine.solr.ReadSolrServerDetails;
 import com.advanceweb.afc.jb.search.engine.solr.SolrJobSearchResultDTO;
 
 public class JobSearchDeleagateImplTest extends ServiceTest{
 	
+	
 	@Autowired
 	private JobSearchDeleagate jobSearchDeleagate ;//= new JobSearchDeleagateImpl();
 
+	private static final String SEARCH_NAME = "basicjobsearch";
+	private static final Map<String, String> PARAM_MAP = new HashMap<String, String>();
+	
+	
 	@Test
 	public void jobSearchTest1() {
 		
-		String searchName = "basicjobsearch";
-		Map<String, String> paramMap = new HashMap<String, String>();
-		paramMap.put("titlesearch", "doctor");
+		PARAM_MAP.put("titlesearch", "doctor");
 		//paramMap.put("", arg1);
 		//paramMap.put("", arg1);
 		long rows = 4;
 		long start = 0;
 		
-		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(searchName, paramMap, rows, start);
+		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(SEARCH_NAME, PARAM_MAP, rows, start);
 		assertTrue("Job Search", jobSearchResultDTO.getSolrJobSearchResultDTO() != null);
 		SolrJobSearchResultDTO solrJobSearchResultDTO = jobSearchResultDTO.getSolrJobSearchResultDTO();
 		List<JobSearchDTO> searchResultList = solrJobSearchResultDTO.getSearchResultList();
@@ -52,15 +44,11 @@ public class JobSearchDeleagateImplTest extends ServiceTest{
 	@Test
 	public void jobSearchTest2() {
 		
-		String searchName = "basicjobsearch";
-		Map<String, String> paramMap = new HashMap<String, String>();
-		paramMap.put("titlesearch", "doctor");
-		//paramMap.put("", arg1);
-		//paramMap.put("", arg1);
+		PARAM_MAP.put("titlesearch", "nurse");
 		long rows = 4;
 		long start = 0;
 		
-		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(searchName, paramMap, rows, start);
+		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(SEARCH_NAME, PARAM_MAP, rows, start);
 		assertTrue("Job Search", jobSearchResultDTO.getSolrJobSearchResultDTO() != null);
 		SolrJobSearchResultDTO solrJobSearchResultDTO = jobSearchResultDTO.getSolrJobSearchResultDTO();
 		List<JobSearchDTO> searchResultList = solrJobSearchResultDTO.getSearchResultList();
@@ -72,15 +60,11 @@ public class JobSearchDeleagateImplTest extends ServiceTest{
 	@Test
 	public void jobSearchTest3() {
 		
-		String searchName = "basicjobsearch";
-		Map<String, String> paramMap = new HashMap<String, String>();
-		paramMap.put("titlesearch", "doctor");
-		//paramMap.put("", arg1);
-		//paramMap.put("", arg1);
+		PARAM_MAP.put("titlesearch", "test");
 		long rows = 0;
 		long start = 0;
 		
-		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(searchName, paramMap, rows, start);
+		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(SEARCH_NAME, PARAM_MAP, rows, start);
 		assertTrue("Job Search", jobSearchResultDTO.getSolrJobSearchResultDTO() != null);
 		SolrJobSearchResultDTO solrJobSearchResultDTO = jobSearchResultDTO.getSolrJobSearchResultDTO();
 		List<JobSearchDTO> searchResultList = solrJobSearchResultDTO.getSearchResultList();
@@ -92,15 +76,11 @@ public class JobSearchDeleagateImplTest extends ServiceTest{
 	@Test
 	public void jobSearchTest4() {
 		
-		String searchName = "basicjobsearch";
-		Map<String, String> paramMap = new HashMap<String, String>();
-		paramMap.put("titlesearch", "");
-		//paramMap.put("", arg1);
-		//paramMap.put("", arg1);
+		PARAM_MAP.put("titlesearch", "");
 		long rows = 4;
 		long start = 0;
 		
-		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(searchName, paramMap, rows, start);
+		JobSearchResultDTO jobSearchResultDTO = jobSearchDeleagate.jobSearch(SEARCH_NAME, PARAM_MAP, rows, start);
 		assertTrue("Job Search", jobSearchResultDTO.getSolrJobSearchResultDTO() == null);
 		SolrJobSearchResultDTO solrJobSearchResultDTO = jobSearchResultDTO.getSolrJobSearchResultDTO();
 		List<JobSearchDTO> searchResultList = solrJobSearchResultDTO.getSearchResultList();
@@ -115,8 +95,6 @@ public class JobSearchDeleagateImplTest extends ServiceTest{
 		String searchName = "";
 		Map<String, String> paramMap = new HashMap<String, String>();
 		paramMap.put("titlesearch", "doctor");
-		//paramMap.put("", arg1);
-		//paramMap.put("", arg1);
 		long rows = 4;
 		long start = 0;
 		
