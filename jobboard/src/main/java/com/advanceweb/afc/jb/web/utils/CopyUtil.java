2abf72232f638ea7a669718526f8b0b0bfaff40b
nishantn <nishantn@localhost>
2012-07-19 12:09:45 +0000

    Removed sysout and added directory for uploaded resume

--- jobboard/src/main/java/com/advanceweb/afc/jb/web/utils/CopyUtil.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/web/utils/CopyUtil.java
@@ -1,14 +1,17 @@
 package com.advanceweb.afc.jb.web.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 public class CopyUtil {
 
 	public static void copy( List<String> li ,String basedirectorypath) throws Exception{
-		
+
 		new CopyUtil().doTheCopy(li,basedirectorypath);
 	}
 
@@ -25,10 +28,52 @@ public class CopyUtil {
 				}
 				fos.flush();
 				fos.close();
+				is.close();
 			}
 		}catch (Exception e) {
 			// TODO: handle exception
 			throw e;
 		}
 	}
+
+
+	public static void Move(String source,String destination)
+	{	
+
+		InputStream inStream = null;
+		OutputStream outStream = null;
+
+		try{
+			File afile =new File(source);
+			//File afile =new File("D:\\JobBoardWorkspc_19_July_2012\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\jobboard\\resources\\images\\Resume_doc.doc");
+			File bfile =new File(destination);
+
+			inStream = new FileInputStream(afile);
+			outStream = new FileOutputStream(bfile);
+
+			byte[] buffer = new byte[1024];
+
+			int length;
+			//copy the file content in bytes 
+			while ((length = inStream.read(buffer)) > 0){
+
+				outStream.write(buffer, 0, length);
+
+			}
+
+			inStream.close();
+			outStream.close();
+
+			//delete the original file
+			afile.delete();
+
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+
 }
