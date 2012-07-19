f35c3aa26ec66bac8aeb3a06a00cacb4fe299091
reeteshrn <reeteshrn@localhost>
2012-07-19 05:07:34 +0000

    Added proper comments

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchResultDTO.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchResultDTO.java
@@ -2,6 +2,12 @@ package com.advanceweb.afc.jb.search.engine.solr;
 
 import java.io.Serializable;
 
+/**
+ * @author ReeteshRN
+ * @version 1.0
+ * @Date 10th July 2012 onwards
+ */
+
 public class JobSearchResultDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
