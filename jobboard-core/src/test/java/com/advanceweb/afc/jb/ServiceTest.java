aadc2943a88ea38b591f5916b664ddaa16195256
anilm <anilm@localhost>
2012-07-20 05:54:28 +0000

    Path issue resolved.

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/ServiceTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/ServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:resources/HibernateSessionFactory.xml" })
+@ContextConfiguration(locations = { "classpath:HibernateSessionFactory.xml" })
 public class ServiceTest extends AbstractJUnit4SpringContextTests {
 
 	@BeforeClass
