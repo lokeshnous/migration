248c5f78f14fa135251375e10f4dc9d8770ff753
sasibhushanam <sasibhushanam@localhost>
2012-07-24 10:34:39 +0000

    Change password

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/ChangePasswordForm.java
@@ -0,0 +1,44 @@
+package com.advanceweb.afc.jb.login.web.controller;
+
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+public class ChangePasswordForm {
+	private String emailId;
+	private String currentPassword;
+	private String newPassword;
+	private String confirmNewPassword;
+	
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getCurrentPassword() {
+		return currentPassword;
+	}
+	public void setCurrentPassword(String currentPassword) {
+		this.currentPassword = currentPassword;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	public String getConfirmNewPassword() {
+		return confirmNewPassword;
+	}
+	public void setConfirmNewPassword(String confirmNewPassword) {
+		this.confirmNewPassword = confirmNewPassword;
+	}
+	
+	
+		
+}
