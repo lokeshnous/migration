dc1bf17dbac65e47dfdc5b483a4bc2e3bc1e09ce
bharatiu <bharatiu@localhost>
2012-07-25 09:12:47 +0000

    Committed for job seeker login task

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/login/service/LoginFormService.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/login/service/LoginFormService.java
@@ -4,7 +4,7 @@ import com.advanceweb.afc.jb.common.LoginFormDTO;
 
 
 /**
- * 
+ * This Interface defines the required methods for login form
  * @author bharatiu
  * @version 1.0
  * @since 19th July 2012
