f38ec98ab6331a80f8073a11e7ef7119d2932404
sasibhushanam <sasibhushanam@localhost>
2012-07-26 14:53:24 +0000

    Registration UI

--- jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMJBCommonConstants.java
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMJBCommonConstants.java
@@ -8,6 +8,9 @@ package com.advanceweb.afc.jb.common.util;
  * @Since 2nd July, 2012
  */
 public interface MMJBCommonConstants {
-	public static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+	public static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-zA-Z]).{6,20})";
 	public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
+	//For DropDowns
+	public static final String EMPLOYMENT_TYPE="EmploymentType";
 }
