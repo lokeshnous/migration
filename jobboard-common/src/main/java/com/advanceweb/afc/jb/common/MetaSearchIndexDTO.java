42c777754940adcb77584d2bb7520523efae6732
reeteshrn <reeteshrn@localhost>
2012-07-26 15:07:08 +0000

    Created DTOs for Entity classes

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/MetaSearchIndexDTO.java
@@ -0,0 +1,57 @@
+package com.advanceweb.afc.jb.common;
+
+public class MetaSearchIndexDTO {
+	
+	private int searchIndexId;
+	
+	private String searchIndexGroup;
+	
+	private String searchIndexName;
+	
+	private String environment;
+	
+	private String searchHost;
+
+	public int getSearchIndexId() {
+		return searchIndexId;
+	}
+
+	public void setSearchIndexId(int searchIndexId) {
+		this.searchIndexId = searchIndexId;
+	}
+
+	public String getSearchIndexGroup() {
+		return searchIndexGroup;
+	}
+
+	public void setSearchIndexGroup(String searchIndexGroup) {
+		this.searchIndexGroup = searchIndexGroup;
+	}
+
+	public String getSearchIndexName() {
+		return searchIndexName;
+	}
+
+	public void setSearchIndexName(String searchIndexName) {
+		this.searchIndexName = searchIndexName;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	public String getSearchHost() {
+		return searchHost;
+	}
+
+	public void setSearchHost(String searchHost) {
+		this.searchHost = searchHost;
+	}
+	
+	
+
+}
