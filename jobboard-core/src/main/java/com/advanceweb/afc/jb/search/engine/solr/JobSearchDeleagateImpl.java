b75ebe8a400be201684d1c110c66c6534c16fd9c
reeteshrn <reeteshrn@localhost>
2012-07-26 15:00:03 +0000

    Implemented Store procedure logic for search

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchDeleagateImpl.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchDeleagateImpl.java
@@ -1,5 +1,6 @@
 package com.advanceweb.afc.jb.search.engine.solr;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
@@ -8,6 +9,10 @@ import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.advanceweb.afc.jb.common.MetaSearchIndexDTO;
+import com.advanceweb.afc.jb.common.MetaSearchInputDTO;
+import com.advanceweb.afc.jb.search.engine.solr.dao.SearchDao;
 
 /**
  * This class has been created for the Solr Server Job search functionalities.
@@ -21,7 +26,7 @@ import org.springframework.stereotype.Service;
 public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 
 	private static final Logger LOGGER = Logger
-	.getLogger("JobSearchDeleagateImpl.class");
+			.getLogger("JobSearchDeleagateImpl.class");
 
 	@Autowired
 	private ReadSolrServerDetails readSSDetails;
@@ -29,9 +34,12 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 	@Autowired
 	@Resource(name = "solrConfiguration")
 	private Properties solrConfiguration;
-	
+
 	@Autowired
 	private SolrJobSearchResult solrJobSearchResult;
+
+	@Autowired
+	private SearchDao searchDao;
 
 	@PostConstruct
 	public void init() {
@@ -50,12 +58,35 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 
 	@Override
 	public JobSearchResultDTO jobSearch(final String searchName,
-			final Map<String, String> paramMap, final long start, final long rows) {
+			final Map<String, String> paramMap, final long start,
+			final long rows) {
 
 		QueryResponse response = null;
 		JobSearchResultDTO jSResultDTO = null;
 		SolrJobSearchResultDTO solrJSResultDTO = null;
 
+		List<MetaSearchInputDTO> mSInputList = searchDao.getParamList(
+				solrConfiguration.getProperty("searchindexname"),
+				solrConfiguration.getProperty("environment"),
+				solrConfiguration.getProperty("searchtypename"));
+
+		List<MetaSearchIndexDTO> srchIndexList = searchDao.getURLQuery(
+				solrConfiguration.getProperty("searchindexname"),
+				solrConfiguration.getProperty("environment"),
+				solrConfiguration.getProperty("searchIndexGroup"));
+
+		paramMap.put("rows", String.valueOf(rows));
+		paramMap.put("start", String.valueOf(start));
+		paramMap.put("querytype", searchName);
+
+		Map<String, StringBuffer> urlParamMap = readSSDetails.getSearchQuery(
+				mSInputList, srchIndexList, paramMap);
+		LOGGER.info("URL QueryString====>>>" + urlParamMap.get("urlString").toString());
+		//LOGGER.info("Param QueryString====>>>" + urlParamMap.get("paramString").toString());
+		
+		paramMap.put("urlQueryString", urlParamMap.get("urlString").toString());
+		//paramMap.put("paramQueryString", urlParamMap.get("paramString").toString());
+		
 		final Map<String, String> serverDetailsMap = readSSDetails
 				.getServerDetails(solrConfiguration);
 		final Map<String, String> solrQueryDetails = readSSDetails
@@ -64,45 +95,43 @@ public class JobSearchDeleagateImpl implements JobSearchDeleagate {
 		final String url = serverDetailsMap.get("serverUrl")
 				+ serverDetailsMap.get("solrservice")
 				+ serverDetailsMap.get("user");
-		
+
 		// Checking whether server url is accessible
 		boolean serverAccessible = readSSDetails.isServerAccessible(url);
 		if (serverAccessible) {
 
-			if (("".equalsIgnoreCase(paramMap.get("keywords"))
-					|| paramMap.get("keywords") == null) 
-					&& ("".equalsIgnoreCase(paramMap.get("cityState"))
-							|| paramMap.get("cityState") == null)
-							&& ("".equalsIgnoreCase(paramMap.get("radius"))
-									|| paramMap.get("radius") == null)) {
+			if (("".equalsIgnoreCase(paramMap.get("keywords")) || paramMap
+					.get("keywords") == null)
+					&& ("".equalsIgnoreCase(paramMap.get("cityState")) || paramMap
+							.get("cityState") == null)
+					&& ("".equalsIgnoreCase(paramMap.get("radius")) || paramMap
+							.get("radius") == null)) {
 
-				LOGGER.debug("Empty Search criteria. Please enter a search criteria to seach jobs.");
+				LOGGER.info("Empty Search criteria. Please enter a search criteria to seach jobs.");
 				return null;
 
 			} else {
 
-				response = readSSDetails.getSolrResponse(
-						serverDetailsMap, solrQueryDetails, paramMap, start,
-						rows);
-				if(response == null){
-					
-					 LOGGER.debug("No Results Found...");
-					 return null;
-					
-				}else{
-					solrJSResultDTO = solrJobSearchResult.getSolrJSResult(response);
+				response = readSSDetails.getSolrResponse(serverDetailsMap,
+						solrQueryDetails, paramMap, start, rows, mSInputList);
+				if (response == null) {
+
+					LOGGER.info("No Results Found...");
+					return null;
+
+				} else {
+					solrJSResultDTO = solrJobSearchResult
+							.getSolrJSResult(response);
 					jSResultDTO = new JobSearchResultDTO();
-					jSResultDTO
-							.setSolrJobSearchResultDTO(solrJSResultDTO);
-	
+					jSResultDTO.setSolrJobSearchResultDTO(solrJSResultDTO);
+
 					return jSResultDTO;
 				}
-				
 
 			}
 
 		} else {
-			 LOGGER.debug("Server url is not correct. Please check the url.");
+			LOGGER.info("Server url is not correct. Please check the url.");
 			return null;
 		}
 
