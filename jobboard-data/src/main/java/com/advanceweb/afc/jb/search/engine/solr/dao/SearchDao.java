48be7e0a3c60a01cd00abbc04be31a9118fa0296
reeteshrn <reeteshrn@localhost>
2012-07-26 15:05:23 +0000

    Created DAO interface for search results

---
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/search/engine/solr/dao/SearchDao.java
@@ -0,0 +1,22 @@
+package com.advanceweb.afc.jb.search.engine.solr.dao;
+
+import java.util.List;
+
+import com.advanceweb.afc.jb.common.MetaSearchIndexDTO;
+import com.advanceweb.afc.jb.common.MetaSearchInputDTO;
+
+public interface SearchDao {
+	
+	/**
+	 * This method is used for getting the parameter lists from the DB for 
+	 * creating the SOLR query.
+	 */
+	List<MetaSearchInputDTO> getParamList(String searchIndexName,String environment, String searchTypeName);
+	
+	/**
+	 * This method is used for getting the rqsults related to Solr server URL.
+	 */
+	
+	List<MetaSearchIndexDTO> getURLQuery(String searchIndexName,String environment, String searchIndexGroup);
+
+}
