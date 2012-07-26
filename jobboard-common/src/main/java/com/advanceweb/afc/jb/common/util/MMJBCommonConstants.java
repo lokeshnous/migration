07f637fa7749961e3deef3a003b49a433ba16154
sasibhushanam <sasibhushanam@localhost>
2012-07-26 08:24:29 +0000

    Adding common constants

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMJBCommonConstants.java
@@ -0,0 +1,13 @@
+package com.advanceweb.afc.jb.common.util;
+
+/**
+ * 
+ * @author Sasibhushana
+ *
+ * @Version 1.0
+ * @Since 2nd July, 2012
+ */
+public interface MMJBCommonConstants {
+	public static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+	public static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+}
