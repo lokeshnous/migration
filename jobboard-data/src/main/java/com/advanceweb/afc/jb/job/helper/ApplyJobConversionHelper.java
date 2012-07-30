dd4d8f1222de5966ccb6f259249cb9441e5de402
princem <princem@localhost>
2012-07-30 05:22:25 +0000

    Updated the file name

---
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/job/helper/ApplyJobConversionHelper.java
@@ -0,0 +1,14 @@
+package com.advanceweb.afc.jb.job.helper;
+
+
+/**
+ * @Author : Prince Mathew
+   @Version: 1.0
+   @Created: Jul 12, 2012
+   @Purpose: This class will work as a converter from AnonymousUserJobApplyDTO to Entity class Object or from Entity class Object to AnonymousUserJobApplyDTO
+ */
+public class ApplyJobConversionHelper {
+	
+	
+	
+}
