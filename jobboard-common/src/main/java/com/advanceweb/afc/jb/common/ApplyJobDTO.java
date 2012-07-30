e60459963320795aba674379b6e333f26dfedb5c
princem <princem@localhost>
2012-07-30 05:23:09 +0000

    Updated the file name

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/ApplyJobDTO.java
@@ -0,0 +1,22 @@
+package com.advanceweb.afc.jb.common;
+
+import java.io.Serializable;
+
+/**
+ * @Author : Prince Mathew
+   @Version: 1.0 
+   @Created: Jul 12, 2012
+   @Purpose: This class will act as a DTO for the Anonymous User to apply for job
+ */
+public class ApplyJobDTO implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+
+	/*private String employerEmail;
+	private String jobTitle;
+	private String jobDescription;*/
+	
+	// currently not using 
+}
