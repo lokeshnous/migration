07bd9e9dd6a97339785fe28c151fdc529d19bf97
reeteshrn <reeteshrn@localhost>
2012-08-01 14:18:16 +0000

    Code review modifications

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDaoImpl.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDaoImpl.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.advanceweb.afc.jb.common.MetaSearchIndexDTO;
-import com.advanceweb.afc.jb.common.MetaSearchInputDTO;
+import com.advanceweb.afc.jb.common.MetaSearchParamDTO;
+import com.advanceweb.afc.jb.common.QueryDTO;
 import com.advanceweb.afc.jb.data.entities.MetaSearchIndex;
-import com.advanceweb.afc.jb.data.entities.MetaSearchInput;
+import com.advanceweb.afc.jb.data.entities.MetaSearchParam;
+import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 
 /**
  * @Author : Reetesh RN
@@ -39,34 +41,43 @@ public class SearchDaoImpl implements SearchDao {
 	/**
 	 * This method is used for getting the parameter lists from the DB for 
 	 * creating the SOLR query.
+	 * @throws JobBoardDataException 
 	 */
-	public List<MetaSearchInputDTO> getParamList(String searchIndexName,
-			String environment, String searchTypeName) {
+	public QueryDTO getSearchQueryDTO(String searchIndexName,
+			String environment, String searchIndexGroup, String searchTypeName) throws JobBoardDataException {
 		
-		List<MetaSearchInputDTO> srchInputList = new ArrayList<MetaSearchInputDTO>();
+		QueryDTO queryDTO = new QueryDTO();
+		List<MetaSearchParamDTO> srchParamDTOList = new ArrayList<MetaSearchParamDTO>();
 		try {
 			@SuppressWarnings("unchecked")
-			
-			List<MetaSearchInput> mSrchInputList =hibernateTemplate.find("SELECT a from  MetaSearchInput a, MetaSearchIndex b, MetaSearchType c  where "+
+			List<MetaSearchParam> mSrchParamList = hibernateTemplate.find("SELECT a from  MetaSearchParam a, MetaSearchIndex b, MetaSearchType c  where "+
 					" a.searchIndexId = b.searchIndexId  and "+
 					"a.searchTypeId = c.searchTypeId and b.searchIndexName = '"+searchIndexName+"' and "+ 
-					"b.environment = '"+environment+"' and c.searchTypeName = '"+searchTypeName+"'");
+					"b.environment = '"+environment+"' and c.searchTypeName = '"+searchTypeName+"'and b.searchIndexGroup = '"+searchIndexGroup+"' order by a.seq");
 			
-			for(MetaSearchInput obj: mSrchInputList){
-				MetaSearchInputDTO mSInputDTO = new MetaSearchInputDTO();
-				mSInputDTO.setInputName(obj.getInputName());
-				//mSInputDTO.setInputValue(obj.getInputValue());
-				
-				LOGGER.debug(mSInputDTO.getInputName());
-				//LOGGER.debug(mSInputDTO.getInputValue());
-				
-				srchInputList.add(mSInputDTO);
+			for(MetaSearchParam obj: mSrchParamList){
+				MetaSearchParamDTO msParamDTO = new MetaSearchParamDTO();
+				msParamDTO.setParameterName(obj.getParameterName());
+				msParamDTO.setParameterValue(obj.getParameterValue());
+				srchParamDTOList.add(msParamDTO);
 			}
+			
+			if(mSrchParamList != null && mSrchParamList.size() > 0){
+				queryDTO.setEnvironment(mSrchParamList.get(0).getMetaSearchIndex().getEnvironment());
+				queryDTO.setSearchIndexName(mSrchParamList.get(0).getMetaSearchIndex().getSearchIndexName());
+				queryDTO.setSearchIndexGroup(mSrchParamList.get(0).getMetaSearchIndex().getSearchIndexGroup());
+				queryDTO.setSearchName(mSrchParamList.get(0).getMetaSearchType().getSearchTypeName());
+				queryDTO.setSearchHost(mSrchParamList.get(0).getMetaSearchIndex().getSearchHost());
+				queryDTO.setmSrchParamList(srchParamDTOList);
+			}
+			
+			LOGGER.info("Search Param List from DAO===>{"+mSrchParamList+"}");
 
 		} catch (HibernateException e) {
-			LOGGER.info(e);
+			LOGGER.debug(e);
+			throw new JobBoardDataException("Error while fetching the SOLR parameters from the Database...");
 		}
-		return srchInputList;
+		return queryDTO;
 	}
 	
 	/**
