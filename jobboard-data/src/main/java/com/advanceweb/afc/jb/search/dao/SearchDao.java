2fcfa25c4d4ba490f8f75c216a95ac3869eeb920
reeteshrn <reeteshrn@localhost>
2012-08-01 14:15:56 +0000

    Code review modifications

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDao.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDao.java
@@ -1,9 +1,7 @@
 package com.advanceweb.afc.jb.search.dao;
 
-import java.util.List;
-
-import com.advanceweb.afc.jb.common.MetaSearchIndexDTO;
-import com.advanceweb.afc.jb.common.MetaSearchInputDTO;
+import com.advanceweb.afc.jb.common.QueryDTO;
+import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 
 public interface SearchDao {
 	
@@ -11,12 +9,8 @@ public interface SearchDao {
 	 * This method is used for getting the parameter lists from the DB for 
 	 * creating the SOLR query.
 	 */
-	List<MetaSearchInputDTO> getParamList(String searchIndexName,String environment, String searchTypeName);
+    QueryDTO getSearchQueryDTO(String searchIndexName,String environment, String searchIndexGroup, String searchTypeName) throws JobBoardDataException;
 	
-	/**
-	 * This method is used for getting the rqsults related to Solr server URL.
-	 */
-	
-	List<MetaSearchIndexDTO> getURLQuery(String searchIndexName,String environment, String searchIndexGroup);
+
 
 }
