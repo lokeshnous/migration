bc54fa2f653ca6927207b413c1b11af4776f96d9
reeteshrn <reeteshrn@localhost>
2012-08-01 13:39:09 +0000

    Code review modifications

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/job/service/impl/JobSearchServiceImpl.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/job/service/impl/JobSearchServiceImpl.java
@@ -4,9 +4,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 import com.advanceweb.afc.jb.search.JobSearchDeleagate;
 import com.advanceweb.afc.jb.search.JobSearchService;
 import com.advanceweb.afc.jb.search.engine.solr.JobSearchResultDTO;
+import com.advanceweb.afc.jb.service.exception.JobBoardServiceException;
+
 import org.springframework.stereotype.Service;
 
 @Service("jobSearchService")
@@ -15,9 +18,16 @@ public class JobSearchServiceImpl implements JobSearchService{
 	@Autowired
 	private JobSearchDeleagate jobSearchDeleagate;
 	
-	@Override
+	/**
+	 * Does the Job Search
+	 * @param searchName
+	 * @param paramMap
+	 * @param rows
+	 * @param start
+	 * @return JobSearchResultDTO
+	 */
 	public JobSearchResultDTO jobSearch(final String searchName,
-			final Map<String, String> paramMap, final long start, final long rows) {
+			final Map<String, String> paramMap, final long start, final long rows) throws JobBoardServiceException, JobBoardDataException {
 		JobSearchResultDTO jobSearchResultDTO = null;
 		jobSearchResultDTO = jobSearchDeleagate.jobSearch(searchName, paramMap, start, rows);
 		return jobSearchResultDTO;
