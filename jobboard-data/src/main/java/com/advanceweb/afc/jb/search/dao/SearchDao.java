aecc70b1b927768bab81fc5c9cbb2b7c05bb6309
reeteshrn <reeteshrn@localhost>
2012-08-02 10:30:29 +0000

    Code review modifications

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDao.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDao.java
@@ -6,9 +6,15 @@ import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 public interface SearchDao {
 	
 	/**
-	 * This method is used for getting the parameter lists from the DB for 
-	 * creating the SOLR query.
+	 * This method is used for getting the parameter lists from the DB for creating the SOLR query.
+	 * @param searchIndexName represents search index name
+	 * @param environment
+	 * @param searchIndexGroup represents search index group
+	 * @param searchTypeName represents search type name
+	 * @return QueryDTO object of QueryDTO
+	 * @throws JobBoardDataException
 	 */
+	
     QueryDTO getSearchQueryDTO(String searchIndexName,String environment, String searchIndexGroup, String searchTypeName) throws JobBoardDataException;
 	
 
