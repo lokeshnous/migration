d68426896097e0e895dd0f52fbe4640a1c403d78
reeteshrn <reeteshrn@localhost>
2012-08-02 10:32:36 +0000

    Code review modifications

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchResultDTO.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/engine/solr/JobSearchResultDTO.java
@@ -1,6 +1,10 @@
 package com.advanceweb.afc.jb.search.engine.solr;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.response.FacetField.Count;
 
 /**
  * @author ReeteshRN
@@ -11,14 +15,43 @@ import java.io.Serializable;
 public class JobSearchResultDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private SolrJobSearchResultDTO solrJobSearchResultDTO;
 	
-	public SolrJobSearchResultDTO getSolrJobSearchResultDTO() {
-		return solrJobSearchResultDTO;
+	private List<JobSearchDTO> searchResultList;
+	private Map<String, List<String>> facetMap;
+	private long totalNumSearchResult;
+	private long start;
+	private long rows;
+	
+	public long getStart() {
+		return start;
 	}
-	public void setSolrJobSearchResultDTO(
-			SolrJobSearchResultDTO solrJobSearchResultDTO) {
-		this.solrJobSearchResultDTO = solrJobSearchResultDTO;
+	public void setStart(long start) {
+		this.start = start;
 	}
+	public long getRows() {
+		return rows;
+	}
+	public void setRows(long rows) {
+		this.rows = rows;
+	}
+	public long getTotalNumSearchResult() {
+		return totalNumSearchResult;
+	}
+	public void setTotalNumSearchResult(long totalNumSearchResult) {
+		this.totalNumSearchResult = totalNumSearchResult;
+	}
+	public List<JobSearchDTO> getSearchResultList() {
+		return searchResultList;
+	}
+	public void setSearchResultList(List<JobSearchDTO> searchResultList) {
+		this.searchResultList = searchResultList;
+	}
+	public Map<String, List<String>> getFacetMap() {
+		return facetMap;
+	}
+	public void setFacetMap(Map<String, List<String>> facetMap) {
+		this.facetMap = facetMap;
+	}
+	
 	
 }
