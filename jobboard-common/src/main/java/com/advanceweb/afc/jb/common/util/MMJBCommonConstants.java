7b03348c2af05bc3a1c3d6445ffb3b1c944de054
reeteshrn <reeteshrn@localhost>
2012-08-03 09:57:37 +0000

    Removed public static final for Solr constants

--- jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMJBCommonConstants.java
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMJBCommonConstants.java
@@ -26,80 +26,84 @@ public interface MMJBCommonConstants {
 	public static final String VISIBILITY_PRIVATE = "Private";
 
 	// For SOLR search
-	public static final String SLASH = "/";
-	public static final String PLUS = "+";
-	public static final String SLASH_SELECT_SLASH = "/select/?";
-	public static final String AMP = "&";
-	public static final String QUESTION_MARK = "?";
-	public static final String EQUAL_TO = "=";
-	public static final String USER = "admin";
-	public static final String SELECT_SLASH_QUESTIONMARK = "select/?";
+	String SLASH = "/";
+	String PLUS = "+";
+	String SLASH_SELECT_SLASH = "/select/?";
+	String AMP = "&";
+	String QUESTION_MARK = "?";
+	String EQUAL_TO = "=";
+	String USER = "admin";
+	String SELECT_SLASH_QUESTIONMARK = "select/?";
 	
-	public static final String KEYWORDS = "keywords";
-	public static final String CITY_STATE = "cityState";
-	public static final String RADIUS = "radius";
-	public static final String SESSION_ID = "sessionid";
+	String KEYWORDS = "keywords";
+	String CITY_STATE = "cityState";
+	String RADIUS = "radius";
+	String SESSION_ID = "sessionid";
 	
-	public static final String SEARCH_SEQ = "search_seq";
-	public static final String QUERY_TYPE = "queryType";
-	public static final String CITY = "city";
-	public static final String STATE = "state";
-	public static final String COMPANY = "company";
-	public static final String POSTED_DT = "posted_dt";
+	String SEARCH_SEQ = "search_seq";
+	String QUERY_TYPE = "queryType";
+	String CITY = "city";
+	String STATE = "state";
+	String COMPANY = "company";
+	String POSTED_DT = "posted_dt";
 	
-	public static final String B_01 = ":b01";
-	public static final String B_02 = ":b02";
-	public static final String B_03 = ":b03";
-	public static final String B_04 = ":b04";
-	public static final String B_05 = ":b05";
-	public static final String B_06 = ":b06";
+	String B_01 = ":b01";
+	String B_02 = ":b02";
+	String B_03 = ":b03";
+	String B_04 = ":b04";
+	String B_05 = ":b05";
+	String B_06 = ":b06";
 	
-	public static final String CAP_COMPANY = "Company";
-	public static final String JOB_TITLE = "JobTitle";
-	public static final String CAP_CITY = "City";
-	public static final String POSTED_DATE = "PostedDate";
-	public static final String APPLY_ONLINE = "ApplyOnline";
-	public static final String BLIND_AD = "BlindAd";
-	public static final String FACILITY_NAME = "FacilityName";
-	public static final String EMAIL_DISPLAY = "EmailDisplay";
-	public static final String EMAIL = "Email";
-	public static final String IS_INTERNATIONAL = "IsInternational";
-	public static final String IS_NATIONAL = "IsNational";
-	public static final String IS_FEATURED = "IsFeatured";
-	public static final String JOB_COUNT = "JobCount";
-	public static final String JOB_ID = "JobId";
-	public static final String JOB_NUMBER = "Job Number";
-	public static final String JOB_GEO = "Job Geo";
-	public static final String JOB_POSITION = "JobPosition";
-	public static final String JOB_GEO_0_LATLON = "jobGeo0LatLon";
-	public static final String JOB_GEO_1_LATLON = "jobGeo1LatLon";
-	public static final String URL_DISPLAY = "URLDisplay";
+	String CAP_COMPANY = "Company";
+	String JOB_TITLE = "JobTitle";
+	String CAP_CITY = "City";
+	String POSTED_DATE = "PostedDate";
+	String APPLY_ONLINE = "ApplyOnline";
+	String BLIND_AD = "BlindAd";
+	String FACILITY_NAME = "FacilityName";
+	String EMAIL_DISPLAY = "EmailDisplay";
+	String EMAIL = "Email";
+	String IS_INTERNATIONAL = "IsInternational";
+	String IS_NATIONAL = "IsNational";
+	String IS_FEATURED = "IsFeatured";
+	String JOB_COUNT = "JobCount";
+	String JOB_ID = "JobId";
+	String JOB_NUMBER = "Job Number";
+	String JOB_GEO = "Job Geo";
+	String JOB_POSITION = "JobPosition";
+	String JOB_GEO_0_LATLON = "jobGeo0LatLon";
+	String JOB_GEO_1_LATLON = "jobGeo1LatLon";
+	String URL_DISPLAY = "URLDisplay";
 	
-	public static final String TOTAL_NO_RECORDS = "TotalNoRecords";
-	public static final String JSON_ROWS = "jsonRows";
+	String TOTAL_NO_RECORDS = "TotalNoRecords";
+	String JSON_ROWS = "jsonRows";
 	
-	public static final String DEF_TYPE = "defType";
-	public static final String SELECT = "select";
-	public static final String SO_TIMEOUT = "sotimeout";
-	public static final String CONNECTION_TIMEOUT = "connectiontimeout";
-	public static final String MAX_CONNECTION_HOST = "maxconnectionperhost";
-	public static final String MAX_TOTAL_CONNECTION = "maxtotalconnection";
-	public static final String FOLLOW_REDIRECTS = "followredirects";
-	public static final String ALLOW_COMPRESSION = "allowcompression";
-	public static final String MAX_RETRIES = "maxretries";
+	String DEF_TYPE = "defType";
+	String SELECT = "select";
+	String SO_TIMEOUT = "sotimeout";
+	String CONNECTION_TIMEOUT = "connectiontimeout";
+	String MAX_CONNECTION_HOST = "maxconnectionperhost";
+	String MAX_TOTAL_CONNECTION = "maxtotalconnection";
+	String FOLLOW_REDIRECTS = "followredirects";
+	String ALLOW_COMPRESSION = "allowcompression";
+	String MAX_RETRIES = "maxretries";
 	
-	public static final String QF = "qf";
-	public static final String PF = "pf";
-	public static final String PS = "ps";
-	public static final String MM = "mm";
-	public static final String BQ = "bq";
-	public static final String BF = "bf";
-	public static final String SORT = "sort";
-	public static final String Q_ALT = "q.alt";
-	public static final String ROWS = "rows";
-	public static final String START = "start";
-	public static final String Q = "q";
+	String QF = "qf";
+	String PF = "pf";
+	String PS = "ps";
+	String MM = "mm";
+	String BQ = "bq";
+	String BF = "bf";
+	String SORT = "sort";
+	String Q_ALT = "q.alt";
+	String ROWS = "rows";
+	String START = "start";
+	String Q = "q";
 
+	//Solr Date pattern
+	
+	String SOLR_DATE_PATTERN = "E MMM dd hh:mm:ss Z yyyy";
+	String REQ_SOLR_DATE_PATTERN = "MMM-dd-yyyy";
 	//For Resume
 	
 	public static final String POST_RESUME = "postresume";
