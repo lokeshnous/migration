9c7b41f6848c4f994f3a5e7e672431f36c4b51d0
reeteshrn <reeteshrn@localhost>
2012-08-03 05:59:34 +0000

    Code review modifications

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/JobSearchService.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/JobSearchService.java
@@ -7,12 +7,13 @@ import com.advanceweb.afc.jb.search.engine.solr.JobSearchResultDTO;
 import com.advanceweb.afc.jb.service.exception.JobBoardServiceException;
 
 public interface JobSearchService{
+	
 	/**
-	 * Does the Job Search
-	 * @param searchName
-	 * @param paramMap
-	 * @param rows
-	 * @param start
+	 * This method is used to do the Job Search by taking the following parameters.
+	 * @param searchName	represents the type of the job search 
+	 * @param paramMap		contains the input parameters from the UI
+	 * @param rows			represents how many rows will be displayed
+	 * @param start			represents the starting point of the search
 	 * @return JobSearchResultDTO
 	 */
 	
