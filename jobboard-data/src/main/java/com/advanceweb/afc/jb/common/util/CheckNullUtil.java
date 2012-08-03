92e6ad45d3699d85d8c57ccaf7d6e662e4573308
reeteshrn <reeteshrn@localhost>
2012-08-03 10:11:17 +0000

    After PMD run

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/common/util/CheckNullUtil.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/common/util/CheckNullUtil.java
@@ -1,12 +1,26 @@
 package com.advanceweb.afc.jb.common.util;
 
+/**
+ * This class has been created as a Utility class
+ * @author Reetesh Ranjan Nayak
+ * @version 1.0
+ * @since 30th July 2012
+ */
+
 public class CheckNullUtil {
 	
+	/**
+	 * This method will check if the passed String is Null or not and 
+	 * depending on the condition it will return the string.
+	 * @param checkStr
+	 * @return
+	 */
 	public static String isNull(String checkStr){
-		if(checkStr == null)
+		if(checkStr == null){
 			return "Not Available";
-		else
+		}else{
 			return checkStr;
+		}
 	}
 	
 
