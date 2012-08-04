f9162784cd73e777b0aad7efba56a0c95221bbfc
reeteshrn <reeteshrn@localhost>
2012-08-04 14:48:21 +0000

    Created for Searching from JP_LOCATIOn table

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/LocationDTO.java
@@ -0,0 +1,67 @@
+package com.advanceweb.afc.jb.common;
+
+public class LocationDTO {
+	
+	
+	private String city;
+	
+	private String state;
+	
+	private String postcode;
+	
+	private float latitude;
+	
+	private float longitude;
+	
+	private String country;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+
+}
