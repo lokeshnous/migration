f170b264efb337550ef51fe74b78674d5e2294ef
sasibhushanam <sasibhushanam@localhost>
2012-08-06 07:45:08 +0000

    Registration updated as per new Change

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/user/helper/RegistrationConversionHelper.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/user/helper/RegistrationConversionHelper.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.advanceweb.afc.jb.common.AddressDTO;
 import com.advanceweb.afc.jb.common.DropDownDTO;
-import com.advanceweb.afc.jb.common.JobSeekerProfileDTO;
 import com.advanceweb.afc.jb.common.JobSeekerRegistrationDTO;
 import com.advanceweb.afc.jb.common.MerProfileAttribDTO;
 import com.advanceweb.afc.jb.common.MerUserDTO;
@@ -37,7 +35,6 @@ public class RegistrationConversionHelper {
 	public MerUser transformMerUserDTOToMerUser(JobSeekerRegistrationDTO dto) {
 
 		MerUserDTO userDTO = dto.getMerUserDTO();		
-		List<MerUserProfile> listProfiles = new ArrayList<MerUserProfile>();
 		
 		MerUser entity = new MerUser();
 		MerUserProfilePK pk = null;
@@ -68,21 +65,17 @@ public class RegistrationConversionHelper {
 					entity.setLastName(attribDTO.getStrLabelValue());
 				}
 				
-//				if(!StringUtils.isEmpty(attribDTO.getStrProfileAttribId())){
-//					pk.setProfileAttribId(Integer.valueOf(attribDTO.getStrProfileAttribId()));
-//				}
+				if(null != attribDTO.getStrProfileAttribId() && !attribDTO.getStrProfileAttribId().isEmpty()){
+					pk.setProfileAttribId(Integer.valueOf(attribDTO.getStrProfileAttribId()));
+				}
 				
 				if(userDTO.getUserId() != 0){
 					entity.setUserId(userDTO.getUserId());
-//					pk.setUserId(userDTO.getUserId());
 				}
 				
-//				profile.setId(pk);
-//				profile.setMerUser(entity);
-//				listProfiles.add(profile);
 			}						
 		}		
-//		entity.setMerUserProfiles(listProfiles);
+
 				
 		return entity;
 
@@ -103,16 +96,21 @@ public class RegistrationConversionHelper {
 			
 			for(MerProfileAttribDTO attribDTO : dto.getAttribList()){
 				
-				if(!MMJBCommonConstants.LABEL_SUSBSCRIPTION.equals(attribDTO.getStrLabelName())){
+				if(!MMJBCommonConstants.LABEL_SUSBSCRIPTION.equals(attribDTO.getStrLabelName()) 
+						&& !MMJBCommonConstants.FIRST_NAME.equals(attribDTO.getStrLabelName())
+						&& !MMJBCommonConstants.MIDDLE_NAME.equals(attribDTO.getStrLabelName())
+						&& !MMJBCommonConstants.LAST_NAME.equals(attribDTO.getStrLabelName())
+						&& !MMJBCommonConstants.EMAIL_ADDRESS.equals(attribDTO.getStrLabelName())){
+					
 					pk = new MerUserProfilePK();
 					MerUserProfile profile = new MerUserProfile();
 					
 					profile.setAttribValue(attribDTO.getStrLabelValue());
-									
-//					if(!StringUtils.isEmpty(attribDTO.getStrProfileAttribId())){
-//						pk.setProfileAttribId(Integer.valueOf(attribDTO.getStrProfileAttribId()));
-//					}
-					
+
+					if(null != attribDTO.getStrProfileAttribId() && !attribDTO.getStrProfileAttribId().isEmpty()){
+						pk.setProfileAttribId(Integer.valueOf(attribDTO.getStrProfileAttribId()));
+					}
+
 					if(user.getUserId() != 0){
 						pk.setUserId(user.getUserId());
 					}
@@ -143,8 +141,7 @@ public class RegistrationConversionHelper {
 			for(MerProfileAttribDTO attribDTO : dto.getAttribList()){
 				
 				if(MMJBCommonConstants.LABEL_SUSBSCRIPTION.equals(attribDTO.getStrLabelName())
-//						&& !StringUtils.isEmpty(attribDTO.getStrLabelValue())
-						){					
+						&& null != attribDTO.getStrLabelValue() && !attribDTO.getStrLabelValue().isEmpty()){					
 					List<String> sellItems = Arrays.asList(attribDTO.getStrLabelValue().split(","));
 					for(String idVal : sellItems){
 						pk = new AdmUserSubscriptionPK();
@@ -163,101 +160,51 @@ public class RegistrationConversionHelper {
 	}
 	
 	
-	
-	
-	/**
-	 * Transform MerUserDTO to entity MerUser	 
-	 * @param dto
-	 * @return
-	 */
-	public MerUserProfile transformMerUserDTOToMerUserProfile(JobSeekerRegistrationDTO dto) {
-		MerUserProfile entity = new MerUserProfile();
-		MerUserDTO userDTO = dto.getMerUserDTO();
-		AddressDTO addDTO = dto.getAddressDTO();
-		JobSeekerProfileDTO profileDTO = dto.getJobSeekerProfileDTO();
-		
-/*		if(addDTO != null){
-			entity.setCity(addDTO.getCity());
-			entity.setCountry(addDTO.getCountry());
-			entity.setState(addDTO.getState());
-			entity.setStreetAddress2(addDTO.getAddress2());
-			entity.setStreetAddress1(addDTO.getAddress1());
-			entity.setPhone(addDTO.getPhone());
-			entity.setIndustry(userDTO.getIndustry());
-			entity.setJobTitle(userDTO.getJobTitle());
-			entity.setProfession(userDTO.getProfession());
-			entity.setSpeciality(userDTO.getSpeciality());	
-		}
-		
-		if(profileDTO != null){
-			entity.setEthinicity(profileDTO.getEthinicity());
-			entity.setVeteranStatus(profileDTO.getVeteranStatus());
-
-		}*/
-		
-		return entity;
-
-	}
-	
-	
 	/**
 	 * Transform MerUser to MerUserDTO
 	 * @param dto
 	 * @return
 	 */
-	public MerUserDTO transformMerUserToMerUserDTO(MerUser entity) {
-		MerUserDTO dto = new MerUserDTO();
-		if (entity != null) {
-			dto.setEmailId(entity.getEmail());
-			dto.setFirstName(entity.getFirstName());
-			dto.setLastName(entity.getLastName());
-			dto.setMiddleName(entity.getMiddleName());
-			dto.setPassword(entity.getPassword());
-//			dto.setJobTitle(entity.getJobTitle());
-//			dto.setProfession(entity.getProfession());
-//			dto.setSpeciality(entity.getSpeciality());
-//			dto.setIndustry(entity.getIndustry());
+	public JobSeekerRegistrationDTO transformMerUserProfilesToDTO(MerUser user, JobSeekerRegistrationDTO jsDTO, 
+			List<MerUserProfile> profiles) {
+		
+		if (null !=jsDTO.getAttribList()) {
+			
+			for(MerProfileAttribDTO attribDTO : jsDTO.getAttribList()){
+				if(MMJBCommonConstants.FIRST_NAME.equalsIgnoreCase(attribDTO.getStrLabelName())){
+					attribDTO.setStrLabelValue(user.getFirstName());
+				}else if(MMJBCommonConstants.LAST_NAME.equalsIgnoreCase(attribDTO.getStrLabelName())){
+					attribDTO.setStrLabelValue(user.getLastName());
+				}else if(MMJBCommonConstants.MIDDLE_NAME.equalsIgnoreCase(attribDTO.getStrLabelName())){
+					attribDTO.setStrLabelValue(user.getMiddleName());
+				}else if(MMJBCommonConstants.EMAIL_ADDRESS.equalsIgnoreCase(attribDTO.getStrLabelName())){
+					attribDTO.setStrLabelValue(user.getEmail());
+				}else{
+					attribDTO.setStrLabelValue(retrieveLabelValue(attribDTO, profiles));
+				}
+			}
 		}
-		return dto;
 
+		return jsDTO;
 	}
 	
 	/**
-	 * Transform MerUser to AddressDTO
-	 * @param dto
+	 * 
+	 * @param attribDTO
+	 * @param profiles
 	 * @return
 	 */
-	public AddressDTO transformMerUserToAddDTO(MerUser entity) {
-		AddressDTO dto = new AddressDTO();
-		if (entity != null) {
-//			dto.setCity(entity.);
-//			dto.setAddress1(address1);
-//			dto.setAddress2(address2);
-//			dto.setCountry(entity.getCountryLocationId());
-//			dto.setPhone(entity.getMobileNo());		
-//			dto.setState(entity.g);
-//			dto.setStreet(entity.get);
-//			dto.setZipCode(entity.getZipCodeLocationId());
+	private String retrieveLabelValue(MerProfileAttribDTO attribDTO, List<MerUserProfile> profiles){
+		
+		if(null != attribDTO){
+			for(MerUserProfile profile : profiles){
+				if(attribDTO.getStrProfileAttribId().equalsIgnoreCase(String.valueOf(profile.getId().getProfileAttribId()))){
+					return profile.getAttribValue();
+				}
+			}
 		}
-		return dto;
-
-	}
-	
-	/**
-	 * Transform MerUser to MerUserDTO
-	 * @param dto
-	 * @return
-	 */
-	public JobSeekerProfileDTO transformMerUserToProfileDTO(MerUser entity) {
-		JobSeekerProfileDTO dto = new JobSeekerProfileDTO();
-		if (entity != null) {
-//			dto.setEmploymentInformation(String.valueOf(entity.getEmpinfoLookupId()));
-//			dto.setEthinicity(String.valueOf(entity.getEthinicityLookupId()));
-//			dto.setGender(String.valueOf(entity.getGender()));
-//			dto.setVeteranStatus(String.valueOf(entity.getVeteranLookupId()));
-		}
-		return dto;
-
+		
+		return null;		
 	}
 	
 	/**
@@ -278,7 +225,6 @@ public class RegistrationConversionHelper {
 				dto.setStrAttribType(entity.getFormType());
 				dto.setStrLabelName(entity.getName());
 				dto.setStrProfileAttribId(String.valueOf(entity.getProfileAttribId()));
-				dto.setStrScreenName(entity.getScreenName());
 				dto.setStrSectionName(entity.getSectionName());
 				dto.setbRequired(entity.getRequired());
 				dto.setStrToolTip(entity.getToolTip());
