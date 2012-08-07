a94185d64bfb886bd300e3e2de8b3bea39d0f963
reeteshrn <reeteshrn@localhost>
2012-08-07 07:26:02 +0000

    After UI Integration and location search implementation, renamed the file name

---
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/search/dao/SearchDAO.java
@@ -0,0 +1,21 @@
+package com.advanceweb.afc.jb.search.dao;
+
+import com.advanceweb.afc.jb.common.QueryDTO;
+import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
+
+public interface SearchDAO {
+	
+	/**
+	 * This method is used for getting the parameter lists from the DB for creating the SOLR query.
+	 * @param searchIndexName represents search index name
+	 * @param environment
+	 * @param searchIndexGroup represents search index group
+	 * @param searchTypeName represents search type name
+	 * @return QueryDTO object of QueryDTO
+	 * @throws JobBoardDataException
+	 */
+	
+    QueryDTO getSearchQueryDTO(String searchIndexName,String environment, String searchIndexGroup, String searchTypeName) throws JobBoardDataException;
+    
+
+}
