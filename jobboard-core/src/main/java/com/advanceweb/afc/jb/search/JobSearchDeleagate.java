5f4a36ce23749655562c5fef92754d234554f9c1
reeteshrn <reeteshrn@localhost>
2012-08-08 13:49:52 +0000

    Autocomplete implemented

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/search/JobSearchDeleagate.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/search/JobSearchDeleagate.java
@@ -1,7 +1,9 @@
 package com.advanceweb.afc.jb.search;
 
+import java.util.List;
 import java.util.Map;
 
+import com.advanceweb.afc.jb.common.LocationDTO;
 import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 import com.advanceweb.afc.jb.search.engine.solr.JobSearchResultDTO;
 import com.advanceweb.afc.jb.service.exception.JobBoardServiceException;
@@ -17,5 +19,14 @@ public interface JobSearchDeleagate {
 	 * @return JobSearchResultDTO
 	 */
 	JobSearchResultDTO jobSearch(String searchName, Map<String, String> paramMap, long start, long rows) throws JobBoardServiceException, JobBoardDataException;
+	
+	/**
+	 * This method will do the location search for city and state
+	 * to display in the auto complete box in the UI.
+	 * @param keywords represents  search word for auto complete
+	 * @return List<LocationDTO> which contains the city and state or postcode 
+	 */
+	
+	List<LocationDTO> locationSearch(String keywords);
 	
 }
