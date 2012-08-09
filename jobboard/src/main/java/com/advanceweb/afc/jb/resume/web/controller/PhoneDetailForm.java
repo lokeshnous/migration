7d9ed442c1705a1608cd26a803c487299caf6136
deviprasadm <deviprasadm@localhost>
2012-08-09 12:55:31 +0000

    initial version

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/resume/web/controller/PhoneDetailForm.java
@@ -0,0 +1,58 @@
+package com.advanceweb.afc.jb.resume.web.controller;
+
+/**
+ * 
+ * @author deviprasadm
+ * @Version 1.0
+ * @Since 9th Aug, 2012
+ */
+public class PhoneDetailForm {
+	private int builderPhoneId;
+	private String phoneNumber;
+	private String phoneType;
+
+	/**
+	 * @return the builderPhoneId
+	 */
+	public int getBuilderPhoneId() {
+		return builderPhoneId;
+	}
+
+	/**
+	 * @param builderPhoneId
+	 *            the builderPhoneId to set
+	 */
+	public void setBuilderPhoneId(int builderPhoneId) {
+		this.builderPhoneId = builderPhoneId;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber
+	 *            the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the phoneType
+	 */
+	public String getPhoneType() {
+		return phoneType;
+	}
+
+	/**
+	 * @param phoneType
+	 *            the phoneType to set
+	 */
+	public void setPhoneType(String phoneType) {
+		this.phoneType = phoneType;
+	}
+}
