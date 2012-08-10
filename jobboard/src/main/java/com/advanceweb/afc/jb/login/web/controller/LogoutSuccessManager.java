cbadda6869edfc964ec5eda722aae002e1c9c2de
princem <princem@localhost>
2012-08-10 10:06:30 +0000

    added the file for custom logout functionality

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LogoutSuccessManager.java
@@ -0,0 +1,29 @@
+package com.advanceweb.afc.jb.login.web.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+public class LogoutSuccessManager extends SimpleUrlLogoutSuccessHandler {
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException,
+			ServletException {
+		System.out.println("******************");
+		response.reset();
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Cache-Control", "must-revalidate");
+		response.setDateHeader("Expires", 0);
+		response.sendRedirect(request.getContextPath() + getDefaultTargetUrl());
+	}
+
+	
+
+}
