49fe3987dd59eba182d0a28da7d6df65608cb23d
reeteshrn <reeteshrn@localhost>
2012-08-14 11:04:00 +0000

    Implementing Edit the saved search

--- jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMUtils.java
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/util/MMUtils.java
@@ -1,7 +1,12 @@
 package com.advanceweb.afc.jb.common.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
 
 import com.advanceweb.afc.jb.common.LocationDTO;
 
@@ -41,5 +46,56 @@ public class MMUtils {
 		}
 		return list;
 	}
+	
+	/**
+	 * This method is used to get the Url splitted into key and value.
+	 * @param url
+	 * @return  Map<String, String>
+	 */
+	public static Map<String, String> getUrlMap(String url){
+		
+		Map<String, String> urlMap = new HashMap<String, String>();
+		StringTokenizer st = new StringTokenizer(url, ";");
+		while(st.hasMoreTokens()) {
+			String key = st.nextToken();
+			
+			StringTokenizer str = new StringTokenizer(key, "=");
+			while(str.hasMoreTokens()){
+				String nameStr = str.nextToken();
+				if(nameStr == null){
+					nameStr = " ";
+				}
+				String valStr = "";
+				if(str.hasMoreTokens()){
+					valStr = str.nextToken();
+					if(valStr == null){
+						valStr = " ";				
+					}
+				}
+				urlMap.put(nameStr, valStr);
+			}
+			
+			String val = st.nextToken();
+			StringTokenizer strFrst = new StringTokenizer(val, "=");
+			while(strFrst.hasMoreTokens()){
+				String nameStr1 = strFrst.nextToken();
+				if(nameStr1 == null){
+					nameStr1 = " ";
+				}
+				String valStr1 = "";
+				if(strFrst.hasMoreTokens()){
+					valStr1 = strFrst.nextToken();
+					if(valStr1 == null){
+						valStr1 = " ";
+					}
+				}
+				
+				urlMap.put(nameStr1, valStr1);
+			}
+		}
+		return urlMap;
+	}
+	
+	
 
 }
