c360edfc4402090c9f6d4f3987bccc4c208ee236
princem <princem@localhost>
2012-08-17 10:57:33 +0000

    updated the file

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginFailureManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginFailureManager.java
@@ -9,10 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
-public class LoginFailureManager extends SimpleUrlAuthenticationFailureHandler{
+import com.advanceweb.afc.jb.common.util.MMJBCommonConstants;
+
+public class LoginFailureManager extends SimpleUrlAuthenticationFailureHandler {
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException,
-	ServletException {
+	public void onAuthenticationFailure(HttpServletRequest request,
+			HttpServletResponse response, AuthenticationException exception)
+			throws IOException, ServletException {
+		String pageValue = request.getParameter(MMJBCommonConstants.PAGE_VALUE);
+
+		if (pageValue != null
+				&& pageValue.equals(MMJBCommonConstants.JOBSEEKER)) {
+			super.setDefaultFailureUrl(MMJBCommonConstants.JOBSEEKER_LOGIN_FAILURE_URL);
+		}
+
+		else if (pageValue != null
+				&& pageValue.equals(MMJBCommonConstants.EMPLOYER)) {
+			super.setDefaultFailureUrl(MMJBCommonConstants.EMPLOYER_LOGIN_FAILURE_URL);
+		} else if (pageValue != null
+				&& pageValue.equals(MMJBCommonConstants.AGENCY)) {
+			super.setDefaultFailureUrl(MMJBCommonConstants.AGENCY_LOGIN_FAILURE_URL);
+		}
+
 		super.onAuthenticationFailure(request, response, exception);
 	}
 
