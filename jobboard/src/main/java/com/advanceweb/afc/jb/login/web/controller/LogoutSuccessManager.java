19f5e412b5babd394e1c979638a820dcfd8dc458
princem <princem@localhost>
2012-08-17 10:58:17 +0000

    updated the file

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LogoutSuccessManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LogoutSuccessManager.java
@@ -7,22 +7,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+import com.advanceweb.afc.jb.common.util.MMJBCommonConstants;
 
 public class LogoutSuccessManager extends SimpleUrlLogoutSuccessHandler {
 
 	@Override
-	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException,
-			ServletException {
+	public void onLogoutSuccess(HttpServletRequest request,
+			HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
 		response.reset();
 		response.setHeader("Cache-Control", "no-cache");
 		response.setHeader("Pragma", "no-cache");
 		response.setHeader("Cache-Control", "no-store");
 		response.setHeader("Cache-Control", "must-revalidate");
 		response.setDateHeader("Expires", 0);
-		response.sendRedirect(request.getContextPath() + getDefaultTargetUrl());
-	}
 
-	
+		if (authentication.getAuthorities().contains(
+				new GrantedAuthorityImpl(MMJBCommonConstants.ROLE_JOB_SEEKER))) {
+			response.sendRedirect(request.getContextPath()
+					+ MMJBCommonConstants.JOBSEEKER_LOGOUT_URL);
+		} else if (authentication.getAuthorities().contains(
+				new GrantedAuthorityImpl(
+						MMJBCommonConstants.ROLE_FACILITY_ADMIN))) {
+			response.sendRedirect(request.getContextPath()
+					+ MMJBCommonConstants.EMPLOYER_LOGOUT_URL);
+		} else if (authentication.getAuthorities()
+				.contains(
+						new GrantedAuthorityImpl(
+								MMJBCommonConstants.ROLE_FACILITY_USER))) {
+			response.sendRedirect(request.getContextPath()
+					+ MMJBCommonConstants.AGENCY_LOGOUT_URL);
+		}
+	}
 
 }
