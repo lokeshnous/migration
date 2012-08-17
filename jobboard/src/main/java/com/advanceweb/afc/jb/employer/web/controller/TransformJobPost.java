06ca39eb3917fd4e10230527d88f965128ddd5f2
sasibhushanam <sasibhushanam@localhost>
2012-08-17 16:27:49 +0000

    Post New Job functionality for employer

--- jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/TransformJobPost.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/TransformJobPost.java
@@ -19,8 +19,8 @@ public class TransformJobPost {
 	
 	public JobPostDTO jobPostFormToJobPostDTO(JobPostForm form){
 		
-/*		JobPostDTO jobPostDTO=new JobPostDTO();
-		jobPostDTO.setJobOwner(form.getJobOwner());
+		JobPostDTO jobPostDTO=new JobPostDTO();
+		/*		jobPostDTO.setJobOwner(form.getJobOwner());
 		jobPostDTO.setCustomerNo(form.getCustomerNo());
 		jobPostDTO.setCompanyName(form.getCompanyName());
 		jobPostDTO.setDisCompanyName(form.getDisCompanyName());
@@ -28,11 +28,11 @@ public class TransformJobPost {
 		jobPostDTO.setJobTitle(form.getJobTitle());
 		jobPostDTO.setJobCity(form.getJobCity());*/
 //		jobPostDTO.setJobState(form.getJobState());
-		jobPostDTO.setJobCountry(form.getJobCountry());
+//		jobPostDTO.setJobCountry(form.getJobCountry());
 //		jobPostDTO.setJobZip(form.getJobZip());
 //		jobPostDTO.setEmpTypeId(form.getEmpTypeId());
-		jobPostDTO.setReqSkills(form.getReqSkills());
-		jobPostDTO.setJobDesc(form.getJobDesc());
+//		jobPostDTO.setReqSkills(form.getReqSkills());
+//		jobPostDTO.setJobDesc(form.getJobDesc());
 //		jobPostDTO.setTrackPixel(form.getTrackingPixel());
 //		jobPostDTO.setJpBrdTemp(form.getJpBrdTemp());
 //		jobPostDTO.setAutoRenew(form.getAutoRenew());
