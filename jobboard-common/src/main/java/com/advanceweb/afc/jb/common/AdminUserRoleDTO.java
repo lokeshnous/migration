479b2ed8e65885313d2e965713845f10f207cc17
princem <princem@localhost>
2012-08-20 15:47:26 +0000

    created for user role

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/AdminUserRoleDTO.java
@@ -0,0 +1,22 @@
+package com.advanceweb.afc.jb.common;
+
+public class AdminUserRoleDTO {
+
+	private int roleId;
+	private String roleName;
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
