b474f55c919578f02a3f45004c4499a827b78452
kartikm <kartikm@localhost>
2012-08-22 16:14:27 +0000

    Add three method for AccountSetting for employer add

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/employer/service/EmployerRegistration.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/employer/service/EmployerRegistration.java
@@ -1,16 +1,21 @@
 package com.advanceweb.afc.jb.employer.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.advanceweb.afc.jb.common.AccountProfileDTO;
 import com.advanceweb.afc.jb.common.EmployerProfileDTO;
 import com.advanceweb.afc.jb.common.MerUserDTO;
 import com.advanceweb.afc.jb.common.ProfileDTO;
 import com.advanceweb.afc.jb.employer.dao.EmployerRegistrationDAO;
 import com.advanceweb.afc.jb.user.ProfileRegistration;
+import com.advanceweb.afc.jb.data.entities.AdmFacilityContact;
 
 /**
  * @author rajeshkb
@@ -18,9 +23,9 @@ import com.advanceweb.afc.jb.user.ProfileRegistration;
  * @created 21-Jun-2012 2:22:44 PM
  */
 @Service("employerRegistration")
-@Transactional(propagation=Propagation.SUPPORTS, readOnly=false)
 public class EmployerRegistration implements ProfileRegistration {
-	
+	private static final Logger LOGGER = Logger
+			.getLogger("EmployerRegistration.class");
 	@Autowired
 	public EmployerRegistrationDAO employerRegistrationDAO;
 
@@ -40,8 +45,6 @@ public class EmployerRegistration implements ProfileRegistration {
 		try {
 			EmployerProfileDTO empProfileDTO = (EmployerProfileDTO) profileDTO;
 			return employerRegistrationDAO.createNewEmployer(empProfileDTO);
-			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -100,5 +103,40 @@ public class EmployerRegistration implements ProfileRegistration {
 	public ProfileDTO getProfileAttributes() {
 		return employerRegistrationDAO.getProfileAttributes();
 	}
+	
 
+	@Override
+	public List<AdmFacilityContact> getEmployeeData(int userId,String contactType) {
+		List<AdmFacilityContact> accountProfileDTO= new ArrayList<AdmFacilityContact>();
+		try {
+			
+			accountProfileDTO=employerRegistrationDAO.getEmployeeData(userId,contactType);
+		} catch (Exception e) {
+			LOGGER.info("Error for employee registration edit");
+		}
+		return accountProfileDTO;
+	}
+
+	@Override
+	public void editEmployeeAccount(AccountProfileDTO apd,int admfacilityid) {
+		employerRegistrationDAO.editEmployeeAccount(apd,admfacilityid);
+		
+	}
+
+	@Override
+	public List<AdmFacilityContact> getEmployeePrimaryKey(int userId,
+			String contactType) {
+		List<AdmFacilityContact> accountProfileDTO= new ArrayList<AdmFacilityContact>();
+		try {
+			
+			accountProfileDTO=employerRegistrationDAO.getEmployeePrimaryKey(userId,contactType);
+		} catch (Exception e) {
+			LOGGER.info("Error for employee registration edit");
+		}
+		return accountProfileDTO;
+	}
+	
+	
+	
+	
 }
