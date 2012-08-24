aa752e31ee6109a60d6e0a8c64d4f9bd78daa90c
harshav <harshav@localhost>
2012-08-24 15:10:53 +0000

    Media files updated in form

--- jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/BrandingTemplateForm.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/BrandingTemplateForm.java
@@ -20,144 +20,80 @@ public class BrandingTemplateForm {
 	private Date createdDate;
 	private String templateName;
 	private String companyOverview;
-	private String mainImage;
+	private String mainImagePath;
 	private CommonsMultipartFile logoFileData;
 	private CommonsMultipartFile mainImageFileData;
-	private String logoFileName;
-	private String logoFilePath;	
-	private String mainImageFileName;
-	private String mainImageFilePath;
 	
 	public long getEmployerId() {
 		return employerId;
 	}
 
-
 	public void setEmployerId(long employerId) {
 		this.employerId = employerId;
 	}
-
 
 	public String getLogoPath() {
 		return logoPath;
 	}
 
-
 	public void setLogoPath(String logoPath) {
 		this.logoPath = logoPath;
 	}
-
 
 	public String getColor() {
 		return color;
 	}
 
-
 	public void setColor(String color) {
 		this.color = color;
 	}
-
 
 	public Date getCreatedDate() {
 		return createdDate;
 	}
 
-
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
-
 
 	public String getTemplateName() {
 		return templateName;
 	}
 
-
 	public void setTemplateName(String templateName) {
 		this.templateName = templateName;
 	}
-
 
 	public String getCompanyOverview() {
 		return companyOverview;
 	}
 
-
 	public void setCompanyOverview(String companyOverview) {
 		this.companyOverview = companyOverview;
 	}
 
-
-	public String getMainImage() {
-		return mainImage;
+	public String getMainImagePath() {
+		return mainImagePath;
 	}
 
-
-	public void setMainImage(String mainImage) {
-		this.mainImage = mainImage;
+	public void setMainImagePath(String mainImagePath) {
+		this.mainImagePath = mainImagePath;
 	}
-
 
 	public CommonsMultipartFile getLogoFileData() {
 		return logoFileData;
 	}
 
-
 	public void setLogoFileData(CommonsMultipartFile logoFileData) {
 		this.logoFileData = logoFileData;
 	}
-
 
 	public CommonsMultipartFile getMainImageFileData() {
 		return mainImageFileData;
 	}
 
-
 	public void setMainImageFileData(CommonsMultipartFile mainImageFileData) {
 		this.mainImageFileData = mainImageFileData;
 	}
-
-
-	public String getLogoFileName() {
-		return logoFileName;
-	}
-
-
-	public void setLogoFileName(String logoFileName) {
-		this.logoFileName = logoFileName;
-	}
-
-
-	public String getLogoFilePath() {
-		return logoFilePath;
-	}
-
-
-	public void setLogoFilePath(String logoFilePath) {
-		this.logoFilePath = logoFilePath;
-	}
-
-
-	public String getMainImageFileName() {
-		return mainImageFileName;
-	}
-
-
-	public void setMainImageFileName(String mainImageFileName) {
-		this.mainImageFileName = mainImageFileName;
-	}
-
-
-	public String getMainImageFilePath() {
-		return mainImageFilePath;
-	}
-
-
-	public void setMainImageFilePath(String mainImageFilePath) {
-		this.mainImageFilePath = mainImageFilePath;
-	}
-
-
-	
 
 }
