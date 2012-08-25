ffd0a1548addcc50fc905e8774791c6afb7a7ef7
princem <princem@localhost>
2012-08-25 10:34:42 +0000

    updated the login functionality depending on the user role

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginSuccessManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginSuccessManager.java
@@ -23,6 +23,7 @@ public class LoginSuccessManager extends SimpleUrlAuthenticationSuccessHandler {
 	public void onAuthenticationSuccess(HttpServletRequest request,
 			HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
+		String pageValue = request.getParameter(MMJBCommonConstants.PAGE_VALUE);
 		response.reset();
 		response.setHeader("Cache-Control", "no-cache");
 		response.setHeader("Pragma", "no-cache");
@@ -38,18 +39,25 @@ public class LoginSuccessManager extends SimpleUrlAuthenticationSuccessHandler {
 		if (authentication.getAuthorities()
 				.contains(
 						new SimpleGrantedAuthority(
-								MMJBCommonConstants.ROLE_JOB_SEEKER))) {
+								MMJBCommonConstants.ROLE_JOB_SEEKER))
+				&& pageValue.equals(MMJBCommonConstants.JOB_SEEKER)) {
 			response.sendRedirect(request.getContextPath()
 					+ "/jobSeeker/jobSeekerDashBoard.html");
 		} else if (authentication.getAuthorities().contains(
-				new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))) {
+				new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))
+				&& pageValue.equals(MMJBCommonConstants.EMPLOYER)) {
 			response.sendRedirect(request.getContextPath()
 					+ "/employer/employerDashBoard.html");
 		} else if (authentication.getAuthorities().contains(
 				new SimpleGrantedAuthority(
-						MMJBCommonConstants.ROLE_FACILITY_GROUP))) {
+						MMJBCommonConstants.ROLE_FACILITY_GROUP))
+				&& pageValue.equals(MMJBCommonConstants.AGENCY)) {
 			response.sendRedirect(request.getContextPath()
 					+ "/agency/agencyDashboard.html");
+		} else {
+			session.invalidate();
+			response.sendRedirect(request.getContextPath()
+					+ "/commonLogin/login.html?error=true&page=" + pageValue);
 		}
 	}
 }
