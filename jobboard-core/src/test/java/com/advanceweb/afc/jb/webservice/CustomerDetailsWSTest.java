fa1e586919f8538873984257613ad3fd90840dd1
sasibhushanam <sasibhushanam@localhost>
2012-08-28 11:20:43 +0000

    Webservie to get customer details

--- jobboard-core/src/test/java/com/advanceweb/afc/jb/webservice/CustomerDetailsWSTest.java
+++ jobboard-core/src/test/java/com/advanceweb/afc/jb/webservice/CustomerDetailsWSTest.java
@@ -1,20 +1,38 @@
 package com.advanceweb.afc.jb.webservice;
 
-import org.springframework.http.HttpEntity;
+/*import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;*/
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.jaxrs.client.WebClient;
 
 import com.advanceweb.afc.jb.ServiceTest;
 
 public class CustomerDetailsWSTest extends ServiceTest{
 
-	RestTemplate template = new RestTemplate();
+	//RestTemplate template = new RestTemplate();
+	
+	public static void main(String[] args){
+
+        CustomerDetailsWSTest customerDetailsWSTest = new CustomerDetailsWSTest();
+
+        customerDetailsWSTest.getCustomerDetails();
+
+	}
+
+
 	
 	public void getCustomerDetails(){
-		String uri = "https://rest.netsuite.com/app/site/hosting/restlet.nl?script=65&deploy=1";
+		/*String uri = "https://rest.netsuite.com/app/site/hosting/restlet.nl?script=65&deploy=1";
 
 		try {
 
@@ -28,5 +46,32 @@ public class CustomerDetailsWSTest extends ServiceTest{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		*/
+		
+		WebClient client = WebClient.create("https://rest.netsuite.com/app/site/hosting/restlet.nl?script=65&deploy=1");
+		client.replaceHeader("Authorization","NLAuth nlauth_account=TSTDRV617993, nlauth_email=dharmarajns@nous.soft.net, nlauth_signature=dharma123, nlauth_role=3");
+        Response r = client.accept("application/json").type("application/json").get();
+
+        String rOut= null;
+
+        try {
+
+                rOut = IOUtils.readStringFromStream((InputStream)r.getEntity());
+
+                
+
+        } catch (IOException e) {
+
+                e.printStackTrace();
+
+                throw new RuntimeException("Failed to get a string represenation of the response",e);
+
+        }
+
+        System.out.println("rOut"+rOut);
+
+        
+
+
 	}
 }
