484adabcaf96175adf3b90e27a8a8dba97cdde79
princem <princem@localhost>
2012-08-29 06:33:32 +0000

    updated the name of the file from TransformEmpoyerBrandTemplate to TransformEmployerBrandTemplate

--- jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/TransformEmployerBrandTemplate.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/TransformEmployerBrandTemplate.java
@@ -9,24 +9,29 @@ import com.advanceweb.afc.jb.common.BrandingTemplateDTO;
 /**
  * 
  * @author Harsha
- *
+ * 
  * @Version 1.0
  * @Since 22nd August, 2012
  */
 
-@Repository("transformEmpoyerBrandTemp")
-public class TransformEmpoyerBrandTemp {
+@Repository("transformEmpoyerBrandTemplate")
+public class TransformEmployerBrandTemplate {
 
-	public BrandingTemplateDTO createEmpBrandTempDTO(BrandingTemplateForm brandingTemplateForm) {
+	public BrandingTemplateDTO createEmpBrandTempDTO(
+			BrandingTemplateForm brandingTemplateForm) {
 
 		BrandingTemplateDTO dto = new BrandingTemplateDTO();
 
-		
-
-		// int userId = (Integer)session.getAttribute(MMJBCommonConstants.USER_ID);
+		// int userId =
+		// (Integer)session.getAttribute(MMJBCommonConstants.USER_ID);
 		dto.setEmployerId(26);
+
+		if (!("".equals(brandingTemplateForm.getTemplateId()))
+				&& brandingTemplateForm.getTemplateId() != null) {
+			dto.setJpBrandTempId(Integer.parseInt(brandingTemplateForm
+					.getTemplateId()));
+		}
 		dto.setFacilityId(1);
-		
 		dto.setLogoPath(brandingTemplateForm.getLogoPath());
 		dto.setColor(brandingTemplateForm.getColor());
 		dto.setCreatedDate(new Date());
@@ -35,17 +40,30 @@ public class TransformEmpoyerBrandTemp {
 		dto.setMainImagePath(brandingTemplateForm.getMainImagePath());
 		dto.setLogoFileData(brandingTemplateForm.getLogoFileData());
 		dto.setMainImageFileData(brandingTemplateForm.getMainImageFileData());
-		
-//		Multi media section
+
+		// Multi media section
 		dto.setTestimony(brandingTemplateForm.getTestimony());
 		dto.setAddImagePath(brandingTemplateForm.getAddImagePath());
 		dto.setAddImageFileData(brandingTemplateForm.getAddImageFileData());
 		dto.setVideoPath(brandingTemplateForm.getVideoPath());
 		dto.setVideoFileData(brandingTemplateForm.getVideoFileData());
-		
+
 		dto.setIsSilverCustomer(brandingTemplateForm.getIsSilverCustomer());
-		
+
 		return dto;
+	}
+
+	public BrandingTemplateForm fromBrandDTOToBrandForm(
+			BrandingTemplateForm form, BrandingTemplateDTO brandingTemplateDTO) {
+		form.setTemplateName(brandingTemplateDTO.getTemplateName());
+		form.setMainImagePath(brandingTemplateDTO.getMainImagePath());
+		form.setLogoPath(brandingTemplateDTO.getLogoPath());
+		form.setColor(brandingTemplateDTO.getColor());
+		form.setCreatedDate(brandingTemplateDTO.getCreatedDate());
+		form.setCompanyOverview(brandingTemplateDTO.getCompanyOverview());
+		form.setLogoFileData(brandingTemplateDTO.getLogoFileData());
+		form.setMainImageFileData(brandingTemplateDTO.getMainImageFileData());
+		return form;
 	}
 
 }
