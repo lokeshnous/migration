376f0f83aab54161587a5064f0976121e2bcc7cf
kartikm <kartikm@localhost>
2012-08-30 15:11:59 +0000

    add another form method Pages have some changes

--- jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/EmployeeAccountForm.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/EmployeeAccountForm.java
@@ -1,290 +1,307 @@
 package com.advanceweb.afc.jb.employer.web.controller;
+
+import com.advanceweb.afc.jb.pgi.web.controller.BillingAddressForm;
+
 /**
  * 
- * @author kartikm
- * Email kartikm@nousinfo.com
- * Date 20.08.2012
- *
+ * @author kartikm 
+ * Email kartikm@nousinfo.com Date 20.08.2012
+ * 
  */
 public class EmployeeAccountForm {
 
 	// Account Address information
-		private String firstName;
-		private String middleName;
-		private String lastName;
-		private String email;
-		private String company;
-		private String streetAddress;
-		private String cityOrTown;
-		private String state;
-		private String country;
-		private String zipCode;
-		private String phone;
+	BillingAddressForm billingAddressForm;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String email;
+	private String company;
+	private String streetAddress;
+	private String cityOrTown;
+	private String state;
+	private String country;
+	private String zipCode;
+	private String phone;
 
+	/**
+	 * @return the billingAddressForm
+	 */
+	public BillingAddressForm getBillingAddressForm() {
+		return billingAddressForm;
+	}
 
-		/**
-		 * @return the firstName
-		 */
-		public String getFirstName() {
-			return firstName;
-		}
+	/**
+	 * @param billingAddressForm
+	 *            the billingAddressForm to set
+	 */
+	public void setBillingAddressForm(BillingAddressForm billingAddressForm) {
+		this.billingAddressForm = billingAddressForm;
+	}
 
-		/**
-		 * @param firstName
-		 *            the firstName to set
-		 */
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
 
-		/**
-		 * @return the middleName
-		 */
-		public String getMiddleName() {
-			return middleName;
-		}
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-		/**
-		 * @param middleName
-		 *            the middleName to set
-		 */
-		public void setMiddleName(String middleName) {
-			this.middleName = middleName;
-		}
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName() {
+		return middleName;
+	}
 
-		/**
-		 * @return the lastName
-		 */
-		public String getLastName() {
-			return lastName;
-		}
+	/**
+	 * @param middleName
+	 *            the middleName to set
+	 */
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 
-		/**
-		 * @param lastName
-		 *            the lastName to set
-		 */
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
 
-		/**
-		 * @return the email
-		 */
-		public String getEmail() {
-			return email;
-		}
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-		/**
-		 * @param email
-		 *            the email to set
-		 */
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
 
-		/**
-		 * @return the company
-		 */
-		public String getCompany() {
-			return company;
-		}
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		/**
-		 * @param company
-		 *            the company to set
-		 */
-		public void setCompany(String company) {
-			this.company = company;
-		}
+	/**
+	 * @return the company
+	 */
+	public String getCompany() {
+		return company;
+	}
 
-		/**
-		 * @return the streetAddress
-		 */
-		public String getStreetAddress() {
-			return streetAddress;
-		}
+	/**
+	 * @param company
+	 *            the company to set
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
 
-		/**
-		 * @param streetAddress
-		 *            the streetAddress to set
-		 */
-		public void setStreetAddress(String streetAddress) {
-			this.streetAddress = streetAddress;
-		}
+	/**
+	 * @return the streetAddress
+	 */
+	public String getStreetAddress() {
+		return streetAddress;
+	}
 
-		/**
-		 * @return the cityOrTown
-		 */
-		public String getCityOrTown() {
-			return cityOrTown;
-		}
+	/**
+	 * @param streetAddress
+	 *            the streetAddress to set
+	 */
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
 
-		/**
-		 * @param cityOrTown
-		 *            the cityOrTown to set
-		 */
-		public void setCityOrTown(String cityOrTown) {
-			this.cityOrTown = cityOrTown;
-		}
+	/**
+	 * @return the cityOrTown
+	 */
+	public String getCityOrTown() {
+		return cityOrTown;
+	}
 
-		/**
-		 * @return the state
-		 */
-		public String getState() {
-			return state;
-		}
+	/**
+	 * @param cityOrTown
+	 *            the cityOrTown to set
+	 */
+	public void setCityOrTown(String cityOrTown) {
+		this.cityOrTown = cityOrTown;
+	}
 
-		/**
-		 * @param state
-		 *            the state to set
-		 */
-		public void setState(String state) {
-			this.state = state;
-		}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
 
-		/**
-		 * @return the country
-		 */
-		public String getCountry() {
-			return country;
-		}
+	/**
+	 * @param state
+	 *            the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
 
-		/**
-		 * @param country
-		 *            the country to set
-		 */
-		public void setCountry(String country) {
-			this.country = country;
-		}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
 
-		/**
-		 * @return the zipCode
-		 */
-		public String getZipCode() {
-			return zipCode;
-		}
+	/**
+	 * @param country
+	 *            the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-		/**
-		 * @param zipCode
-		 *            the zipCode to set
-		 */
-		public void setZipCode(String zipCode) {
-			this.zipCode = zipCode;
-		}
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
 
-		/**
-		 * @return the phone
-		 */
-		public String getPhone() {
-			return phone;
-		}
+	/**
+	 * @param zipCode
+	 *            the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 
-		/**
-		 * @param phone
-		 *            the phone to set
-		 */
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
 
-		/* (non-Javadoc)
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result
-					+ ((cityOrTown == null) ? 0 : cityOrTown.hashCode());
-			result = prime * result
-					+ ((company == null) ? 0 : company.hashCode());
-			result = prime * result
-					+ ((country == null) ? 0 : country.hashCode());
-			result = prime * result + ((email == null) ? 0 : email.hashCode());
-			result = prime * result
-					+ ((firstName == null) ? 0 : firstName.hashCode());
-			result = prime * result
-					+ ((lastName == null) ? 0 : lastName.hashCode());
-			result = prime * result
-					+ ((middleName == null) ? 0 : middleName.hashCode());
-			result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-			result = prime * result + ((state == null) ? 0 : state.hashCode());
-			result = prime * result
-					+ ((streetAddress == null) ? 0 : streetAddress.hashCode());
-			result = prime * result
-					+ ((zipCode == null) ? 0 : zipCode.hashCode());
-			return result;
-		}
+	/**
+	 * @param phone
+	 *            the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-		/* (non-Javadoc)
-		 * @see java.lang.Object#equals(java.lang.Object)
-		 */
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			EmployeeAccountForm other = (EmployeeAccountForm) obj;
-			if (cityOrTown == null) {
-				if (other.cityOrTown != null)
-					return false;
-			} else if (!cityOrTown.equals(other.cityOrTown))
-				return false;
-			if (company == null) {
-				if (other.company != null)
-					return false;
-			} else if (!company.equals(other.company))
-				return false;
-			if (country == null) {
-				if (other.country != null)
-					return false;
-			} else if (!country.equals(other.country))
-				return false;
-			if (email == null) {
-				if (other.email != null)
-					return false;
-			} else if (!email.equals(other.email))
-				return false;
-			if (firstName == null) {
-				if (other.firstName != null)
-					return false;
-			} else if (!firstName.equals(other.firstName))
-				return false;
-			if (lastName == null) {
-				if (other.lastName != null)
-					return false;
-			} else if (!lastName.equals(other.lastName))
-				return false;
-			if (middleName == null) {
-				if (other.middleName != null)
-					return false;
-			} else if (!middleName.equals(other.middleName))
-				return false;
-			if (phone == null) {
-				if (other.phone != null)
-					return false;
-			} else if (!phone.equals(other.phone))
-				return false;
-			if (state == null) {
-				if (other.state != null)
-					return false;
-			} else if (!state.equals(other.state))
-				return false;
-			if (streetAddress == null) {
-				if (other.streetAddress != null)
-					return false;
-			} else if (!streetAddress.equals(other.streetAddress))
-				return false;
-			if (zipCode == null) {
-				if (other.zipCode != null)
-					return false;
-			} else if (!zipCode.equals(other.zipCode))
-				return false;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((cityOrTown == null) ? 0 : cityOrTown.hashCode());
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+				+ ((middleName == null) ? 0 : middleName.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result
+				+ ((streetAddress == null) ? 0 : streetAddress.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		
-		
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeeAccountForm other = (EmployeeAccountForm) obj;
+		if (cityOrTown == null) {
+			if (other.cityOrTown != null)
+				return false;
+		} else if (!cityOrTown.equals(other.cityOrTown))
+			return false;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (middleName == null) {
+			if (other.middleName != null)
+				return false;
+		} else if (!middleName.equals(other.middleName))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (streetAddress == null) {
+			if (other.streetAddress != null)
+				return false;
+		} else if (!streetAddress.equals(other.streetAddress))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+
 }
