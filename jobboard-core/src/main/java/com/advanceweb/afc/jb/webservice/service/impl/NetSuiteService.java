17066babd3ff469bf0cb8d461a931f8118b7918f
reeteshrn <reeteshrn@localhost>
2012-09-05 07:08:41 +0000

    Added service layers for WS call to NetSuite

---
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/webservice/service/impl/NetSuiteService.java
@@ -0,0 +1,19 @@
+package com.advanceweb.afc.jb.webservice.service.impl;
+
+
+/**
+ * This service interface helps to call the different WebServices 
+ * from NetSuite.
+ * @author Reetesh R N
+ * @version 1.0
+ * @since 04 Sept 2012
+ * 
+ */
+
+public interface NetSuiteService {
+	
+	
+	public String createCustomer(Object jsonCustomer);
+	
+
+}
