63b01b67bf47fd7d96f8156a318cf5ff49d25ea7
princem <princem@localhost>
2012-09-05 09:58:04 +0000

    removed modifiers which are implied by the context

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/jobseeker/service/JobSeekerSubscriptionService.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/jobseeker/service/JobSeekerSubscriptionService.java
@@ -22,13 +22,13 @@ public interface JobSeekerSubscriptionService {
 	 * @return
 	 */
 
-	public boolean saveJobSeekerSubscription(List<JobSeekerSubscriptionsDTO> listSubsDTO, int userId);
+	boolean saveJobSeekerSubscription(List<JobSeekerSubscriptionsDTO> listSubsDTO, int userId);
 	
 	/**
 	 * To get current subscriptions of the user
 	 * @param userId
 	 * @return
 	 */
-	public List<JobSeekerSubscriptionsDTO> getCurrentSubscriptions(int userId);
+	List<JobSeekerSubscriptionsDTO> getCurrentSubscriptions(int userId);
 
 }
