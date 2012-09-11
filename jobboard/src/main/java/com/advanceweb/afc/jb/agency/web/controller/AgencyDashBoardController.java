59ec32e70998d62c789cbdf2c387bab624406d6e
srikanthvk <srikanthvk@localhost>
2012-09-11 10:38:32 +0000

    for add and manage employer module in agency dashboard

--- jobboard/src/main/java/com/advanceweb/afc/jb/agency/web/controller/AgencyDashBoardController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/agency/web/controller/AgencyDashBoardController.java
@@ -1,7 +1,12 @@
 package com.advanceweb.afc.jb.agency.web.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,16 +26,23 @@ import com.advanceweb.afc.jb.common.AccountBillingDTO;
 import com.advanceweb.afc.jb.common.AccountProfileDTO;
 import com.advanceweb.afc.jb.common.AdmFacilityContactDTO;
 import com.advanceweb.afc.jb.common.CountryDTO;
+import com.advanceweb.afc.jb.common.EmployerProfileDTO;
 import com.advanceweb.afc.jb.common.StateDTO;
 import com.advanceweb.afc.jb.common.util.MMJBCommonConstants;
+import com.advanceweb.afc.jb.data.entities.AdmFacility;
 import com.advanceweb.afc.jb.employer.service.EmloyerRegistartionService;
 import com.advanceweb.afc.jb.employer.web.controller.EmployeeAccountForm;
+import com.advanceweb.afc.jb.employer.web.controller.EmployerProfileAttribForm;
+import com.advanceweb.afc.jb.employer.web.controller.EmployerRegistrationForm;
+import com.advanceweb.afc.jb.employer.web.controller.EmployerRegistrationValidation;
 //import com.advanceweb.afc.jb.employer.web.controller.EmployerRegistrationValidation;
 import com.advanceweb.afc.jb.employer.web.controller.TransformEmployerRegistration;
 import com.advanceweb.afc.jb.lookup.service.PopulateDropdowns;
 import com.advanceweb.afc.jb.pgi.service.PaymentGatewayService;
 import com.advanceweb.afc.jb.pgi.web.controller.BillingAddressForm;
 import com.advanceweb.afc.jb.pgi.web.controller.TransformPaymentMethod;
+import com.advanceweb.afc.jb.user.ProfileRegistration;
+
 //import com.advanceweb.afc.jb.user.ProfileRegistration;
 
 /**
@@ -59,17 +72,28 @@ public class AgencyDashBoardController {
 	private TransformPaymentMethod transformPaymentMethod;
 	@Autowired
 	private PopulateDropdowns populateDropdownsService;
-	//@Autowired
-	//private ProfileRegistration employerRegistration;
+
+	@Autowired
+	private ProfileRegistration employerRegistration;
+	@Autowired
+	EmployerRegistrationValidation registerValidation;
+
+	@Autowired
+	private ProfileRegistration agencyRegistration;
+
+	// @Autowired
+	// private ProfileRegistration employerRegistration;
 
 	@RequestMapping("/agencyDashboard")
 	public ModelAndView displayDashBoard() {
 		return new ModelAndView(_FORM_VIEW);
 	}
+
 	/**
 	 * This method is called to Account Setting update page and
 	 * editAccountSetting method take Bean class binding result from Jsp pages
-	 * Seession for UserId and FacilityId. 
+	 * Seession for UserId and FacilityId.
+	 * 
 	 * @author kartikm
 	 * @param model
 	 * @return true
@@ -87,8 +111,9 @@ public class AgencyDashBoardController {
 				int admfacilityid = listProfAttribForms.getFacilityContactId();
 				if (!validateEmailPattern(employeeAccountForm.getEmail())) {
 					return MMJBCommonConstants.EMAIL_MESSAGE;
-				} else if (listProfAttribForms.getEmail().toString().equals(employeeAccountForm.getEmail())) {
-					//return MMJBCommonConstants.EMAIL_NULL_MESSAGE;
+				} else if (listProfAttribForms.getEmail().toString()
+						.equals(employeeAccountForm.getEmail())) {
+					// return MMJBCommonConstants.EMAIL_NULL_MESSAGE;
 				} else if (!validatePhonePattern(employeeAccountForm.getPhone())) {
 					return MMJBCommonConstants.PHONE_NO;
 				} else if (null == employeeAccountForm.getPhone()) {
@@ -99,13 +124,12 @@ public class AgencyDashBoardController {
 
 				isUpdated = empRegService.editUser(dto, admfacilityid, userId,
 						MMJBCommonConstants.PRIMARY);
-				if(isUpdated){
+				if (isUpdated) {
 					LOGGER.info("This is Account Addresss edite option done successfully");
-				}else{
+				} else {
 					return MMJBCommonConstants.ERROR_STRING;
 				}
-				
-				
+
 			}
 
 		} catch (Exception e) {
@@ -118,7 +142,8 @@ public class AgencyDashBoardController {
 	/**
 	 * This method is called to Billing Setting update page and
 	 * editBillingSetting method take Bean class binding result from Jsp pages
-	 * Seession for UserId and FacilityId. 
+	 * Seession for UserId and FacilityId.
+	 * 
 	 * @author kartikm
 	 * @param model
 	 * @return true
@@ -151,7 +176,8 @@ public class AgencyDashBoardController {
 				LOGGER.info("This is Billing Addresss edite option done successfully");
 
 			} else {
-				if (listProfAttribForms.getEmail().toString().equals(employeeBillingForm.getEmail())) {
+				if (listProfAttribForms.getEmail().toString()
+						.equals(employeeBillingForm.getEmail())) {
 					return MMJBCommonConstants.EMAIL_NULL_MESSAGE;
 				}
 				BillingAddressForm billingAddressForm = employeeBillingForm.billingAddressForm;
@@ -176,7 +202,8 @@ public class AgencyDashBoardController {
 
 	/**
 	 * This method is called to Account Setting display page call from dash
-	 * board of employer 
+	 * board of employer
+	 * 
 	 * @author kartikm
 	 * @param model
 	 * @return true
@@ -216,7 +243,7 @@ public class AgencyDashBoardController {
 			/**
 			 * this is for billing pages
 			 */
-			//int count = 0;
+			// int count = 0;
 			AdmFacilityContactDTO listBillingForms = empRegService
 					.getEmployeePrimaryKey(userId, MMJBCommonConstants.BILLING);
 			if ((listBillingForms.getCount() > 0)) {
@@ -288,6 +315,222 @@ public class AgencyDashBoardController {
 		Pattern pattern = Pattern.compile(MMJBCommonConstants.MOBILE_PATTERN);
 		Matcher matcher = pattern.matcher(phoneNumber);
 		return matcher.matches();
+	}
+
+	@RequestMapping(value = "/addEmployer", method = RequestMethod.GET)
+	public ModelAndView addEmployer(HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		EmployerRegistrationForm empRegisterForm = new EmployerRegistrationForm();
+
+		EmployerProfileDTO registerDTO = (EmployerProfileDTO) employerRegistration
+				.getProfileAttributes();
+		List<EmployerProfileAttribForm> listProfAttribForms = transformEmpReg
+				.transformDTOToProfileAttribForm(registerDTO, null);
+		empRegisterForm.setListProfAttribForms(listProfAttribForms);
+		model.addObject("empRegisterForm", empRegisterForm);
+		// List<CountryDTO> countryList = populateDropdownsService
+		// .getCountryList();
+		// List<StateDTO> stateList = populateDropdownsService.getStateList();
+		// model.addObject("countryList", countryList);
+		// model.addObject("stateList", stateList);
+		model.setViewName("agencyAddEmployer");
+		return model;
+	}
+
+	// @RequestMapping(value = "/manageEmployers", method = RequestMethod.GET)
+	// public ModelAndView manageEmployers() {
+	// ModelAndView model = new ModelAndView();
+	// List<String> employersList = new ArrayList<String>();
+	// model.setViewName("agencyManageEmployers");
+	// return model;
+	//
+	// }
+
+	@RequestMapping(value = "/editEmployer", method = RequestMethod.GET)
+	public ModelAndView editEmployer(
+			@RequestParam("employerName") String employerName) {
+		ModelAndView model = new ModelAndView();
+		try {
+			EmployerRegistrationForm empRegisterForm = new EmployerRegistrationForm();
+
+			List<CountryDTO> countryList = populateDropdownsService
+					.getCountryList();
+
+			List<StateDTO> stateList = populateDropdownsService.getStateList();
+			EmployerProfileDTO registerDTO = (EmployerProfileDTO) employerRegistration
+					.getProfileAttributes();
+			List<EmployerProfileAttribForm> listProfAttribForms = transformEmpReg
+					.transformDTOToProfileAttribForm(registerDTO, null);
+			empRegisterForm.setListProfAttribForms(listProfAttribForms);
+
+			Map profAttribFormsMap = populateDropdownsService
+					.getEmployerDetails(employerName);
+			model.setViewName("agencyEditEmployer");
+
+			if (profAttribFormsMap != null && !profAttribFormsMap.isEmpty()) {
+				empRegisterForm
+						.setFirstName(profAttribFormsMap.get("name") != null ? profAttribFormsMap
+								.get("name").toString() : "");
+				empRegisterForm
+						.setStreet(profAttribFormsMap.get("street") != null ? profAttribFormsMap
+								.get("street").toString() : "");
+
+				empRegisterForm
+						.setZipCode(profAttribFormsMap.get("postcode") != null ? profAttribFormsMap
+								.get("postcode").toString() : "");
+
+				empRegisterForm
+						.setState(profAttribFormsMap.get("state") != null ? profAttribFormsMap
+								.get("state").toString() : "");
+
+				empRegisterForm
+						.setCountry(profAttribFormsMap.get("country") != null ? profAttribFormsMap
+								.get("country").toString() : "");
+				empRegisterForm
+						.setCity(profAttribFormsMap.get("city") != null ? profAttribFormsMap
+								.get("city").toString() : "");
+				empRegisterForm.setUserId(Integer.parseInt(profAttribFormsMap
+						.get("facilityId").toString()));
+			}
+			empRegisterForm.setListProfAttribForms(listProfAttribForms);
+
+			model.addObject("countryList", countryList);
+			model.addObject("stateList", stateList);
+			model.addObject("listProfAttribForms", profAttribFormsMap);
+			model.addObject("empRegisterForm", empRegisterForm);
+
+		} catch (Exception e) {
+			System.out.println("Error For controller");
+		}
+
+		return model;
+	}
+
+	@RequestMapping(value = "/getEmployerNamesList", method = RequestMethod.GET, headers = "Accept=*/*")
+	public @ResponseBody
+	List<String> getEmployerNamesList(@RequestParam("term") String query) {
+
+		return populateDropdownsService.getEmployerNamesList(query);
+
+	}
+
+	@RequestMapping(value = "/getEmployerDetails")
+	@ResponseBody
+	public Map getEmployerDetails(@RequestParam("name") String employerName) {
+		System.out.println("hi");
+		Map empForm = populateDropdownsService.getEmployerDetails(employerName);
+
+		return empForm;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/addEmployerDetails", method = RequestMethod.POST)
+	public ModelAndView addEmployerDetails(
+			EmployerRegistrationForm employerRegistrationForm,
+			BindingResult result, HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		try {
+
+			AccountProfileDTO dto = transformEmpReg
+					.transformEmployerFormToDto(employerRegistrationForm);
+
+			agencyRegistration.addEmployer(dto);
+
+			model.setViewName("employerDashboard");
+
+			return model;
+		} catch (Exception e) {
+			model.setViewName("employerDashboard");
+			return model;
+		}
+	}
+
+	@RequestMapping(value = "/manageEmployers", method = RequestMethod.GET)
+	public ModelAndView manageEmployers(HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		try {
+
+			int agencyUserId = (Integer) session.getAttribute("userId");
+
+			List<AdmFacility> assocEmplyrsNames = agencyRegistration
+					.getAssocEmployerNames(agencyUserId);
+
+			model.addObject("assocEmplyrsNames", assocEmplyrsNames);
+			model.setViewName("agencyManageEmployers");
+			// model.addObject("employerRegForm", employerRegForm);
+
+		} catch (Exception e) {
+			System.out.println("Error For controller");
+		}
+
+		return model;
+
+	}
+
+	@RequestMapping(value = "/saveEmployerDetails", method = RequestMethod.GET)
+	public ModelAndView saveEmployerDetails(
+			EmployerRegistrationForm employerRegistrationForm,
+			BindingResult result) {
+		ModelAndView model = new ModelAndView();
+		try {
+
+			AccountProfileDTO dto = transformEmpReg
+					.transformEmployerFormToDto(employerRegistrationForm);
+
+			agencyRegistration.saveEmployerDetails(dto);
+
+			model.setViewName("employerDashboard");
+
+			return model;
+		} catch (Exception e) {
+			model.setViewName("employerDashboard");
+			return model;
+		}
+
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/deleteAssocEmployer", method = RequestMethod.GET)
+	public ModelAndView deleteAssocEmployer(
+			@RequestParam("facilityId") String facilityId, HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		try {
+			int userId = (Integer) session.getAttribute("userId");
+			agencyRegistration.deleteAssocEmployer(facilityId, userId);
+
+			model.setViewName("employerDashboard");
+
+			return model;
+		} catch (Exception e) {
+			model.setViewName("employerDashboard");
+			return model;
+		}
+	}
+
+	@RequestMapping(value = "/getAssocEmployersByState", method = RequestMethod.GET)
+	public ModelAndView getAssocEmployersByState(HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		int agencyUserId = (Integer) session.getAttribute("userId");
+		Map<String, List<AdmFacility>> emplyrsByState = new HashMap<String, List<AdmFacility>>();
+		Set<String> stateList = new HashSet<String>();
+
+		List<AdmFacility> assocEmplyrsNames = agencyRegistration
+				.getAssocEmployerNames(agencyUserId);
+		for (AdmFacility assocEmplyr : assocEmplyrsNames) {
+			String state = assocEmplyr.getState();
+			if (stateList.add(state)) {
+				List<AdmFacility> emplyrs = new ArrayList<AdmFacility>();
+				emplyrs.add(assocEmplyr);
+				emplyrsByState.put(state, emplyrs);
+			} else {
+				emplyrsByState.get(state).add(assocEmplyr);
+			}
+		}
+
+		model.addObject("emplyrsByState", emplyrsByState);
+		model.setViewName("agencyDashboard");
+		return model;
+
 	}
 
 }
