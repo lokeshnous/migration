de5d50c640f13d779c2bd7018efe241098938368
kartikm <kartikm@localhost>
2012-09-12 12:17:09 +0000

    Admin Job post Active and Inactive done in Dao interface

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/employer/dao/JobPostDAO.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/employer/dao/JobPostDAO.java
@@ -40,4 +40,12 @@ public interface JobPostDAO {
 	 * @return JobPostDTO
 	 */
 	List<JobPostDTO> retrieveAllJobPostByADvSearch(int advSearchId);
+	/**
+	 * 
+	 * @param apd
+	 * @param jobId
+	 * @return boolean value
+	 * @throws JobBoardServiceException
+	 */
+	public boolean jobSaveByAdmin(JobPostDTO apd, int jobId);
 }
