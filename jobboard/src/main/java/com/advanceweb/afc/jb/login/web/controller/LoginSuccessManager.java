f007425e1012f369da09ace1c8b74e0972bc4b88
manishd <manishd@localhost>
2012-09-13 20:13:57 +0000

    Refactor and updated as per PMD rule.

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginSuccessManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginSuccessManager.java
@@ -28,6 +28,7 @@ public class LoginSuccessManager extends SimpleUrlAuthenticationSuccessHandler {
 	public void onAuthenticationSuccess(HttpServletRequest request,
 			HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
+
 		String pageValue = request.getParameter(MMJBCommonConstants.PAGE_VALUE);
 		response.reset();
 		response.setHeader("Cache-Control", "no-cache");
@@ -36,64 +37,137 @@ public class LoginSuccessManager extends SimpleUrlAuthenticationSuccessHandler {
 		response.setHeader("Cache-Control", "must-revalidate");
 		response.setDateHeader("Expires", 0);
 		UserDTO user = loginService.getUser(authentication.getName());
+
 		HttpSession session = request.getSession(false);
 		session.setAttribute(MMJBCommonConstants.USER_ID, user.getUserId());
 		session.setAttribute(MMJBCommonConstants.USER_NAME, user.getFirstName()
 				+ " " + user.getLastName());
 		session.setAttribute(MMJBCommonConstants.USER_EMAIL, user.getEmailId());
-		if (authentication.getAuthorities()
+
+		if (isJobSeeker(authentication, pageValue)) {
+
+			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
+
+				/**
+				 * if the user already registered
+				 */
+				sendRedirect(request, response,
+						"/jobSeeker/jobSeekerDashBoard.html");
+
+			} else {
+				/**
+				 * if the logged in user is new
+				 */
+				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
+				sendRedirect(request, response,
+						"/jobseekerregistration/createJobSeekerCreateYrAcct.html");
+
+			}
+
+		} else if (isFacility(authentication, pageValue)) {
+
+			/**
+			 * Added to put facility id in the session
+			 */
+			EmployerInfoDTO infoDTO = loginService.facilityDetails(user
+					.getUserId());
+			session.setAttribute(MMJBCommonConstants.FACILITY_ID,
+					infoDTO.getFacilityId());
+
+			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
+				sendRedirect(request, response,
+						"/employer/employerDashBoard.html");
+
+			} else {
+
+				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
+				sendRedirect(request, response,
+						"/employerRegistration/employerregistration.html");
+			}
+
+		} else if (isFacilityGroup(authentication, pageValue)) {
+
+			/**
+			 * Added to put facility id in the session
+			 */
+			EmployerInfoDTO infoDTO = loginService.facilityDetails(user
+					.getUserId());
+			session.setAttribute(MMJBCommonConstants.FACILITY_ID,
+					infoDTO.getFacilityId());
+
+			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
+
+				sendRedirect(request, response, "/agency/agencyDashboard.html");
+
+			} else {
+				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
+				sendRedirect(request, response,
+						"/agencyRegistration/agencyregistration.html");
+			}
+		} else {
+
+			session.invalidate();
+			sendRedirect(request, response,
+					"/commonLogin/login.html?error=true&page=" + pageValue);
+
+		}
+	}
+
+	/**
+	 * Method to redirect to the target URL
+	 * 
+	 * @param request
+	 * @param response
+	 * @param target
+	 * @throws IOException
+	 */
+	private void sendRedirect(HttpServletRequest request,
+			HttpServletResponse response, String target) throws IOException {
+
+		response.sendRedirect(request.getContextPath() + target);
+	}
+
+	/**
+	 * Method return true if authenticated by FACILITY GROUP otherwise false.
+	 * 
+	 * @param authentication
+	 * @param pageValue
+	 * @return
+	 */
+	private boolean isFacilityGroup(Authentication authentication,
+			String pageValue) {
+		return authentication.getAuthorities().contains(
+				new SimpleGrantedAuthority(
+						MMJBCommonConstants.ROLE_FACILITY_GROUP))
+				&& pageValue.equals(MMJBCommonConstants.AGENCY);
+	}
+
+	/**
+	 * Method return true if authenticated by FACILITY otherwise false.
+	 * 
+	 * @param authentication
+	 * @param pageValue
+	 * @return
+	 */
+	private boolean isFacility(Authentication authentication, String pageValue) {
+		return authentication.getAuthorities().contains(
+				new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))
+				&& pageValue.equals(MMJBCommonConstants.EMPLOYER);
+	}
+
+	/**
+	 * Method return true if authenticated by JOB SEEKER otherwise false.
+	 * 
+	 * @param authentication
+	 * @param pageValue
+	 * @return
+	 */
+	private boolean isJobSeeker(Authentication authentication, String pageValue) {
+
+		return authentication.getAuthorities()
 				.contains(
 						new SimpleGrantedAuthority(
 								MMJBCommonConstants.ROLE_JOB_SEEKER))
-				&& pageValue.equals(MMJBCommonConstants.JOB_SEEKER)) {
-			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
-				response.sendRedirect(request.getContextPath()
-						+ "/jobSeeker/jobSeekerDashBoard.html");// if the user
-																// already
-																// registered
-			} else {// if the logged in user is new
-				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
-				response.sendRedirect(request.getContextPath()
-						+ "/jobseekerregistration/createJobSeekerCreateYrAcct.html");
-			}
-
-		} else if (authentication.getAuthorities().contains(
-				new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))
-				&& pageValue.equals(MMJBCommonConstants.EMPLOYER)) {
-			// Added to put facility id in the session
-			EmployerInfoDTO infoDTO = loginService.facilityDetails(user
-					.getUserId());
-			session.setAttribute(MMJBCommonConstants.FACILITY_ID,
-					infoDTO.getFacilityId());
-			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
-				response.sendRedirect(request.getContextPath()
-						+ "/employer/employerDashBoard.html");
-			} else {
-				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
-				response.sendRedirect(request.getContextPath()
-						+ "/employerRegistration/employerregistration.html");
-			}
-		} else if (authentication.getAuthorities().contains(
-				new SimpleGrantedAuthority(
-						MMJBCommonConstants.ROLE_FACILITY_GROUP))
-				&& pageValue.equals(MMJBCommonConstants.AGENCY)) {
-			// Added to put facility id in the session
-			EmployerInfoDTO infoDTO = loginService.facilityDetails(user
-					.getUserId());
-			session.setAttribute(MMJBCommonConstants.FACILITY_ID,
-					infoDTO.getFacilityId());
-			if (profileRegistration.validateProfileAttributes(user.getUserId())) {
-				response.sendRedirect(request.getContextPath()
-						+ "/agency/agencyDashboard.html");
-			} else {
-				session.setAttribute(MMJBCommonConstants.USER_DTO, user);
-				response.sendRedirect(request.getContextPath()
-						+ "/agencyRegistration/agencyregistration.html");
-			}
-		} else {
-			session.invalidate();
-			response.sendRedirect(request.getContextPath()
-					+ "/commonLogin/login.html?error=true&page=" + pageValue);
-		}
+				&& pageValue.equals(MMJBCommonConstants.JOB_SEEKER);
 	}
 }
