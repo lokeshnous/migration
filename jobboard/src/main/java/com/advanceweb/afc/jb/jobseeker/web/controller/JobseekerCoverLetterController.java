9a51e7bee1f99d945ae8b833d3302d8c5ecc45f0
kartikm <kartikm@localhost>
2012-09-14 14:33:00 +0000

    Jobseeker cover letter save option done here in controller class

--- jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobseekerCoverLetterController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobseekerCoverLetterController.java
@@ -3,13 +3,21 @@
  */
 package com.advanceweb.afc.jb.jobseeker.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+import com.advanceweb.afc.jb.common.ResCoverLetterDTO;
+import com.advanceweb.afc.jb.jobseeker.service.CoverLetterService;
 
 
 
@@ -19,15 +27,50 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 @RequestMapping("/jobSeekerCoverLetter")
+@SessionAttributes("resCoverLetterForm")
 @Scope("session")
 public class JobseekerCoverLetterController {
 	private static final Logger LOGGER = Logger
 			.getLogger("JobseekerCoverLetterController.class");
-	
+	@Autowired
+	CoverLetterService coverLetterService;
 	@RequestMapping(value = "/createCoverLetter", method = RequestMethod.GET)
 	public ModelAndView jobSeekerCoverPage(ModelMap map) {
+		ResCoverLetterForm resCoverLetterForm=new ResCoverLetterForm();
 		ModelAndView model = new ModelAndView();
+		model.addObject(resCoverLetterForm);
 		model.setViewName("jobseekerCreateCoverLetter");
 		return model;
 	}
+	/**
+	 * @author kartikm
+	 * This is the save method call when date change and save button 
+	 * click then it is Change from active, inactive, Draft, Expired 
+	 * @param request
+	 * @param session
+	 * @param jobPostform
+	 * @param result
+	 * @return
+	 */
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "/jobseekerCoverLetterSub", method = RequestMethod.POST)
+	public String getJobPostDetailsSave(HttpServletRequest request,
+			HttpSession session, ResCoverLetterForm resCoverLetterForm,BindingResult result) {
+
+		try{
+			int userId = (Integer) session.getAttribute("userId");
+			ResCoverLetterDTO dto=new ResCoverLetterDTO();
+			dto.setName(resCoverLetterForm.getName());
+			dto.setCoverletterText(resCoverLetterForm.getCoverletterText());
+			dto.setActive(resCoverLetterForm.getActive());
+			dto.setUserId(userId);
+			coverLetterService.coverLetterSaveByjobSeeker(dto);
+		}catch (Exception e) {
+			LOGGER.info("Manager Edit Job Posting Search Option");
+		}
+		return "";
+	}
+	
 }
