7fb627aab69ce64faee276054d84544051315de2
princem <princem@localhost>
2012-10-19 15:10:13 +0000

    fixed the code review issues

--- jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobSeekerJobDetailController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobSeekerJobDetailController.java
@@ -5,14 +5,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.advanceweb.afc.jb.agency.web.controller.AgencyDashBoardController;
 import com.advanceweb.afc.jb.common.AppliedJobDTO;
 import com.advanceweb.afc.jb.common.util.MMJBCommonConstants;
+import com.advanceweb.afc.jb.exception.JobBoardException;
 import com.advanceweb.afc.jb.jobseeker.service.JobSeekerJobDetailService;
 
 /**
@@ -29,67 +32,95 @@ public class JobSeekerJobDetailController {
 
 	@Autowired
 	private JobSeekerJobDetailService jobSeekerJobDetailService;
+	private static final Logger LOGGER = Logger
+			.getLogger(JobSeekerJobDetailController.class);
 
+	/**
+	 * this method is used to get the pop up to display all the jobs applied by the corresponding job seeker
+	 * @param Map model
+	 *  @param HttpSession session
+	 * @return ModelAndView
+	 */
 	@RequestMapping(value = "/viewAppliedJob")
 	public ModelAndView getAppliedJob(HttpSession session, Map model) {
+		try{
 		List<AppliedJobDTO> appliedJobDTOList = jobSeekerJobDetailService
 				.getAppliedJobs((Integer) session
 						.getAttribute(MMJBCommonConstants.USER_ID));
 		model.put("appliedJobDTOList", appliedJobDTOList);
+	}catch (JobBoardException e) {
+		LOGGER.debug("Error while fetching all the applied jobs of the corresponding job seeker"
+				+ e);
+	}
 		return new ModelAndView("jobseekerviewappliedjobspopup");
 	}
 
 	/**
-	 * delete action for applied job
-	 * 
-	 * @param model
-	 * @return
+	 * this method is used to delete the applied job of the corresponding job seeker and display the remaining jobs in the pop up
+	 *  @param HttpSession session
+	 *  @param int appliedJobId
+	 *  @param Map model
+	 * @return ModelAndView
 	 */
 	@SuppressWarnings({ "unused" })
 	@RequestMapping(value = "/deleteAppliedJob")
 	public ModelAndView deleteAppliedJob(HttpSession session,
-			@RequestParam("appliedJobId") int jobId, Map model) {
-		boolean result = jobSeekerJobDetailService.updateAppliedSavedJobs(jobId);
+			@RequestParam("appliedJobId") int appliedJobId, Map model) {
+		try{
+		boolean result = jobSeekerJobDetailService.updateAppliedSavedJobs(appliedJobId);
 		List<AppliedJobDTO> appliedJobDTOList = jobSeekerJobDetailService
 				.getAppliedJobs((Integer) session
 						.getAttribute(MMJBCommonConstants.USER_ID));
 		model.put("appliedJobDTOList", appliedJobDTOList);
+	}catch (JobBoardException e) {
+			LOGGER.debug("Error while updating the delete date of the applied job "
+					+ e);
+		}
 		return new ModelAndView("jobseekerviewappliedjobspopup");
 	}
 
 	/**
-	 * delete action for Saved job
-	 * 
-	 * @param model
-	 * @return
+	 * this method is used to delete the saved job of the corresponding job seeker and display the remaining jobs in the pop up
+	 *  @param HttpSession session
+	 *  @param int jobId
+	 *   * @param Map model
+	 * @return ModelAndView
 	 */
 	@SuppressWarnings({ "unused" })
 	@RequestMapping(value = "/deleteSavedJob")
 	public ModelAndView deleteSavedJob(HttpSession session,
 			@RequestParam("appliedJobId") int jobId, Map model) {
-
+		try{
 		boolean result = jobSeekerJobDetailService.updateAppliedSavedJobs(jobId);
 		List<AppliedJobDTO> savedJobDTOList = jobSeekerJobDetailService
 				.getSavedJobs((Integer) session
 						.getAttribute(MMJBCommonConstants.USER_ID));
 		model.put("savedJobDTOList", savedJobDTOList);
-
+	}catch (JobBoardException e) {
+		LOGGER.debug("Error while updating the delete date of the saved job "
+				+ e);
+	}
 		return new ModelAndView("jobseekermysavedjobspopup");
 
 	}
 
 	/**
-	 * to get Saved Job
-	 * 
-	 * @param model
-	 * @return
+	 * this method is used to get the pop up to display all the jobs saved by the corresponding job seeker
+	 * @param Map model
+	 *  @param HttpSession session
+	 * @return ModelAndView
 	 */
 	@RequestMapping(value = "/viewSavedJob")
 	public ModelAndView getSavedJob(HttpSession session, Map model) {
+		try{
 		List<AppliedJobDTO> savedJobDTOList = jobSeekerJobDetailService
 				.getSavedJobs((Integer) session
 						.getAttribute(MMJBCommonConstants.USER_ID));
 		model.put("savedJobDTOList", savedJobDTOList);
+	}catch (JobBoardException e) {
+		LOGGER.debug("Error while fetching all the saved jobs of the corresponding job seeker "
+				+ e);
+	}
 		return new ModelAndView("jobseekermysavedjobspopup");
 	}
 	
