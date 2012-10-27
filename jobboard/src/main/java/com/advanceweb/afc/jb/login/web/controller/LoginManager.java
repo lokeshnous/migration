38ba26622c621a5b0d4177be6c178b35626980c4
princem <princem@localhost>
2012-10-27 15:11:06 +0000

    updated the file to add the implementation to restrict the remember me functionality for admin

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/LoginManager.java
@@ -3,6 +3,7 @@ package com.advanceweb.afc.jb.login.web.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.util.StringUtils;
 
 import com.advanceweb.afc.jb.common.EmployerInfoDTO;
 import com.advanceweb.afc.jb.common.UserDTO;
@@ -35,15 +37,26 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 			HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
 		String pageValue = request.getParameter(MMJBCommonConstants.PAGE_VALUE);
-		String socialSignUp =(String)request.getAttribute("socialSignUp");
+		String socialSignUp = (String) request.getAttribute("socialSignUp");
 		UserDTO user = loginService.getUser(authentication.getName());
 		HttpSession session;
-		if(authentication instanceof UsernamePasswordAuthenticationToken)
-		{ session = request.getSession(false);}
-		else{session = request.getSession();}
-		
-		if(user.isAdmin()){
-			session.setAttribute(MMJBCommonConstants.FACILITY_FULL_ACCESS,MMJBCommonConstants.FACILITY_FULL_ACCESS);
+		if (authentication instanceof UsernamePasswordAuthenticationToken) {
+			session = request.getSession(false);
+		} else {
+			session = request.getSession();
+		}
+
+		if (authentication.getAuthorities().contains(
+				new SimpleGrantedAuthority(
+						MMJBCommonConstants.ROLE_MERION_ADMIN))) {
+			session.setAttribute(MMJBCommonConstants.FACILITY_FULL_ACCESS,
+					MMJBCommonConstants.FACILITY_FULL_ACCESS);
+			String cookieName = MMJBCommonConstants.SPRING_SECURITY_REMEMBER_ME_COOKIE;
+			Cookie cookie = new Cookie(cookieName, null);
+			cookie.setMaxAge(0);
+			cookie.setPath(StringUtils.hasLength(request.getContextPath()) ? request
+					.getContextPath() : "/");
+			response.addCookie(cookie);
 		}
 		session.setAttribute(MMJBCommonConstants.USER_ID, user.getUserId());
 		session.setAttribute(MMJBCommonConstants.USER_NAME, user.getFirstName()
@@ -51,7 +64,7 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 		session.setAttribute(MMJBCommonConstants.USER_EMAIL, user.getEmailId());
 
 		if (isJobSeeker(authentication, pageValue)) {
-			redirectJobSeeker(user,request,response,session);
+			redirectJobSeeker(user, request, response, session);
 		} else if (isFacility(authentication, pageValue)) {
 
 			/**
@@ -63,7 +76,7 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 					infoDTO.getFacilityId());
 			session.setAttribute(MMJBCommonConstants.COMPANY_EMP,
 					infoDTO.getCustomerName());
-			redirectFacility(user,request,response,session);
+			redirectFacility(user, request, response, session);
 		} else if (isFacilitySystem(authentication, pageValue)) {
 
 			/**
@@ -73,13 +86,16 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 					.getUserId());
 			session.setAttribute(MMJBCommonConstants.FACILITY_ID,
 					infoDTO.getFacilityId());
-			redirectFacilitySystem(user,request,response,session);
+			redirectFacilitySystem(user, request, response, session);
 		} else {
-			boolean socalLogin=false;
-			if(socialSignUp!=null){socalLogin=true;}
+			boolean socalLogin = false;
+			if (socialSignUp != null) {
+				socalLogin = true;
+			}
 			session.invalidate();
 			sendRedirect(request, response,
-					"/commonLogin/login.html?error=true&page=" + pageValue+"&socalLogin="+socalLogin);
+					"/commonLogin/login.html?error=true&page=" + pageValue
+							+ "&socalLogin=" + socalLogin);
 
 		}
 	}
@@ -107,16 +123,15 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 	 */
 	private boolean isFacilitySystem(Authentication authentication,
 			String pageValue) {
-		if(authentication instanceof RememberMeAuthenticationToken){
+		if (authentication instanceof RememberMeAuthenticationToken) {
 			return authentication.getAuthorities().contains(
 					new SimpleGrantedAuthority(
 							MMJBCommonConstants.ROLE_FACILITY_SYSTEM));
-		}
-		else{
-		return authentication.getAuthorities().contains(
-				new SimpleGrantedAuthority(
-						MMJBCommonConstants.ROLE_FACILITY_SYSTEM))
-				&& pageValue.equals(MMJBCommonConstants.AGENCY);
+		} else {
+			return authentication.getAuthorities().contains(
+					new SimpleGrantedAuthority(
+							MMJBCommonConstants.ROLE_FACILITY_SYSTEM))
+					&& pageValue.equals(MMJBCommonConstants.AGENCY);
 		}
 	}
 
@@ -129,20 +144,21 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 	 * @return
 	 */
 	private boolean isFacility(Authentication authentication, String pageValue) {
-		if(authentication instanceof RememberMeAuthenticationToken){
+		if (authentication instanceof RememberMeAuthenticationToken) {
 			return authentication.getAuthorities().contains(
-					new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))
+					new SimpleGrantedAuthority(
+							MMJBCommonConstants.ROLE_FACILITY))
 					|| authentication.getAuthorities().contains(
 							new SimpleGrantedAuthority(
 									MMJBCommonConstants.ROLE_FACILITY_GROUP));
-		}
-		else{
-		return (authentication.getAuthorities().contains(
-				new SimpleGrantedAuthority(MMJBCommonConstants.ROLE_FACILITY))
-				|| authentication.getAuthorities().contains(
-						new SimpleGrantedAuthority(
-								MMJBCommonConstants.ROLE_FACILITY_GROUP)))
-				&& pageValue.equals(MMJBCommonConstants.EMPLOYER);
+		} else {
+			return (authentication.getAuthorities().contains(
+					new SimpleGrantedAuthority(
+							MMJBCommonConstants.ROLE_FACILITY)) || authentication
+					.getAuthorities().contains(
+							new SimpleGrantedAuthority(
+									MMJBCommonConstants.ROLE_FACILITY_GROUP)))
+					&& pageValue.equals(MMJBCommonConstants.EMPLOYER);
 		}
 	}
 
@@ -154,29 +170,33 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 	 * @return
 	 */
 	private boolean isJobSeeker(Authentication authentication, String pageValue) {
-		if(authentication instanceof RememberMeAuthenticationToken){
-	return authentication.getAuthorities()
-	.contains(
-			new SimpleGrantedAuthority(
-					MMJBCommonConstants.ROLE_JOB_SEEKER));
+		if (authentication instanceof RememberMeAuthenticationToken) {
+			return authentication.getAuthorities().contains(
+					new SimpleGrantedAuthority(
+							MMJBCommonConstants.ROLE_JOB_SEEKER));
+		} else {
+			return authentication.getAuthorities().contains(
+					new SimpleGrantedAuthority(
+							MMJBCommonConstants.ROLE_JOB_SEEKER))
+					&& pageValue.equals(MMJBCommonConstants.JOB_SEEKER);
 		}
-		else{
-		return authentication.getAuthorities()
-				.contains(
-						new SimpleGrantedAuthority(
-								MMJBCommonConstants.ROLE_JOB_SEEKER))
-				&& pageValue.equals(MMJBCommonConstants.JOB_SEEKER);}
 	}
-	
+
 	/**
-	 * Method to redirect job seeker to the corresponding page depending on the condition
+	 * Method to redirect job seeker to the corresponding page depending on the
+	 * condition
 	 * 
-	 * @param UserDTO user
+	 * @param UserDTO
+	 *            user
 	 * @param HttpServletRequest
-	 * @param HttpServletResponse response
-	 * @param HttpSession session
+	 * @param HttpServletResponse
+	 *            response
+	 * @param HttpSession
+	 *            session
 	 */
-	private void redirectJobSeeker(UserDTO user,HttpServletRequest request,HttpServletResponse response,HttpSession session)throws IOException, ServletException {
+	private void redirectJobSeeker(UserDTO user, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws IOException, ServletException {
 		if (profileRegistration.validateProfileAttributes(user.getUserId())) {
 
 			/**
@@ -206,21 +226,26 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 
 		}
 	}
+
 	/**
-	 * Method to redirect facility to the corresponding page depending on the condition
+	 * Method to redirect facility to the corresponding page depending on the
+	 * condition
 	 * 
-	 * @param UserDTO user
+	 * @param UserDTO
+	 *            user
 	 * @param EmployerInfoDTO
 	 * @param HttpServletRequest
-	 * @param HttpServletResponse response
-	 * @param HttpSession session
+	 * @param HttpServletResponse
+	 *            response
+	 * @param HttpSession
+	 *            session
 	 */
-	private void redirectFacility(UserDTO user,HttpServletRequest request,HttpServletResponse response,HttpSession session)throws IOException, ServletException {
-		 
-		if (profileRegistration
-				.validateProfileAttributes(user.getUserId())) {
-			sendRedirect(request, response,
-					"/employer/employerDashBoard.html");
+	private void redirectFacility(UserDTO user, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session)
+			throws IOException, ServletException {
+
+		if (profileRegistration.validateProfileAttributes(user.getUserId())) {
+			sendRedirect(request, response, "/employer/employerDashBoard.html");
 
 		} else {
 
@@ -229,16 +254,20 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 					"/employerRegistration/employerregistration.html");
 		}
 
-		}
-	
+	}
+
 	/**
-	 * Method to redirect facility system to the corresponding page depending on the condition
+	 * Method to redirect facility system to the corresponding page depending on
+	 * the condition
 	 * 
-	 * @param UserDTO user
+	 * @param UserDTO
+	 *            user
 	 * @param EmployerInfoDTO
 	 * @param HttpServletRequest
-	 * @param HttpServletResponse response
-	 * @param HttpSession session
+	 * @param HttpServletResponse
+	 *            response
+	 * @param HttpSession
+	 *            session
 	 */
 	private void redirectFacilitySystem(UserDTO user,
 			HttpServletRequest request, HttpServletResponse response,
@@ -255,5 +284,4 @@ public class LoginManager extends SimpleUrlAuthenticationSuccessHandler {
 		}
 
 	}
-	}
-
+}
