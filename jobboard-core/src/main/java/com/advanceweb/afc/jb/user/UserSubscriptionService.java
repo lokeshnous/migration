3a010f56c285f4ab49ffff7674a72911984c5bb1
amitkumar <amitkumar@localhost>
2012-10-29 09:40:32 +0000

    updated user/UserSubscriptionService.java adding new functionality

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/user/UserSubscriptionService.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/user/UserSubscriptionService.java
@@ -2,6 +2,7 @@ package com.advanceweb.afc.jb.user;
 
 import java.util.List;
 
+import com.advanceweb.afc.jb.common.DropDownDTO;
 import com.advanceweb.afc.jb.common.UserSubscriptionsDTO;
 
 /**
@@ -64,4 +65,10 @@ public interface UserSubscriptionService {
 	 */
 	boolean saveFacilitySubscription(List<UserSubscriptionsDTO> listSubsDTO,
 			int facilityId);
+	
+	
+	List<DropDownDTO> getSubscriptionscheck(int userId);
+	List<DropDownDTO> getSubscriptionsdigital(int userId);
+	
+	List<DropDownDTO> getSubscriptionsletter(int userId);
 }
