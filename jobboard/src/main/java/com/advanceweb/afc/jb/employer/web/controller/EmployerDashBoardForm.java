fb584426d0cc1fbace73fab181f5fd223109f21e
amitkumar <amitkumar@localhost>
2012-10-30 13:36:54 +0000

    This files are required to commit with respect to Metrics functionality.

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/employer/web/controller/EmployerDashBoardForm.java
@@ -0,0 +1,45 @@
+package com.advanceweb.afc.jb.employer.web.controller;
+
+/**
+ * 
+ * @author Bharati Umarani
+ * @version 1.0
+ * @since 21st sept, 2012
+ * 
+ */
+
+public class EmployerDashBoardForm {
+
+	private String selEmployer;
+	private int clicks;
+	
+
+	/**
+	 * @return the selEmployer
+	 */
+	public String getSelEmployer() {
+		return selEmployer;
+	}
+
+	/**
+	 * @param selEmployer
+	 *            the selEmployer to set
+	 */
+	public void setSelEmployer(String selEmployer) {
+		this.selEmployer = selEmployer;
+	}
+
+	/**
+	 * @return the clicks
+	 */
+	public int getClicks() {
+		return clicks;
+	}
+
+	/**
+	 * @param clicks the clicks to set
+	 */
+	public void setClicks(int clicks) {
+		this.clicks = clicks;
+	}
+}
