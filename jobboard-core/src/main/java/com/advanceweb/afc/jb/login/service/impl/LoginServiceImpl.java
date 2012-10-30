8b24251afb0ac8b8355d70b3de6146ccf5a1d86b
amitkumar <amitkumar@localhost>
2012-10-30 11:54:01 +0000

    updated login/service/impl/LoginServiceImpl.java is added is new functionality

--- jobboard-core/src/main/java/com/advanceweb/afc/jb/login/service/impl/LoginServiceImpl.java
+++ jobboard-core/src/main/java/com/advanceweb/afc/jb/login/service/impl/LoginServiceImpl.java
@@ -1,14 +1,15 @@
 package com.advanceweb.afc.jb.login.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.advanceweb.afc.jb.common.LoginDTO;
 import com.advanceweb.afc.jb.common.UserDTO;
 import com.advanceweb.afc.jb.common.UserRoleDTO;
+import com.advanceweb.afc.jb.data.entities.JpJobStat;
 import com.advanceweb.afc.jb.data.exception.JobBoardDataException;
 import com.advanceweb.afc.jb.login.dao.LoginFormDAO;
 import com.advanceweb.afc.jb.login.service.LoginService;
@@ -24,9 +25,6 @@ import com.advanceweb.afc.jb.user.dao.UserDao;
 
 @Service("loginService")
 public class LoginServiceImpl implements LoginService {
-
-	private static final Logger LOGGER = Logger
-			.getLogger(LoginServiceImpl.class);
 
 	@Autowired
 	private LoginFormDAO loginFormDAO;
@@ -77,32 +75,69 @@ public class LoginServiceImpl implements LoginService {
 		try {
 			userDAO.saveNewPWD(emailAddress, tempassword);
 		} catch (JobBoardDataException e) {
-			LOGGER.info("Error occurred while saving the generated password"
-					+ e);
+			// TODO Auto-generated catch block
 		}
 	}
 
-	@Override
-	public void updateSocialProfileId(int userId, String profileId,int profileAttrId)throws JobBoardServiceException{
-		try {
-		userDAO.updateSocialProfileId(userId, profileId,profileAttrId);
-		} catch (JobBoardDataException e) {
-			LOGGER.info("Error occurred while updating the social profile id"
-					+ e);
-			throw new JobBoardServiceException("Error occurred while updating the social profile id"+ e);
-			
-		}	}
+	/**
+	 * This method to get the Date range data
+	 * 
+	 * @param startFrom
+	 * @param endFrom
+	 * @param selEmployerId
+	 * 
+	 */
 
 	@Override
-	public UserDTO getUserBySocialProfileId(String socialProfileId)throws JobBoardServiceException{
-		try {
-		return	userDAO.getUserBySocialProfileId(socialProfileId);
-	} catch (JobBoardDataException e) {
-		LOGGER.info("Error occurred while fetching the user based on the social profile id"
-				+ e);
-		throw new JobBoardServiceException("Error occurred while fetching the user based on the social profile id"+ e);
-		
-	}	
+	public List<JpJobStat> employerMetrics(Date startFrom, Date endFrom,
+			int selEmployerId) {
+		// TODO Auto-generated method stub
+		return loginFormDAO.employerMetrics(startFrom, endFrom, selEmployerId);
 	}
 
+	/**
+	 * This method to get the active job Posting
+	 * 
+	 * @param facilityId
+	 * 
+	 */
+
+	@Override
+	public int getactivejobposting(int facilityId)
+			throws JobBoardServiceException {
+		// TODO Auto-generated method stub
+		return loginFormDAO.getactivejobposting(facilityId);
+	}
+
+	/**
+	 * This method is used to get the total count of employer
+	 * 
+	 * @return
+	 * @throws JobBoardServiceException
+	 */
+	public long getEmployerCount() throws JobBoardServiceException {
+		long returnVal = 0;
+		try {
+			returnVal = userDAO.getEmployerCount();
+		} catch (JobBoardDataException jde) {
+			throw new JobBoardServiceException(
+					"Error occured while getting the Result from Database"
+							+ jde);
+		}
+		return returnVal;
+	}
+
+	@Override
+	public void updateSocialProfileId(int userId, String profileId,
+			int profileAttrId) throws JobBoardServiceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public UserDTO getUserBySocialProfileId(String socialProfileId)
+			throws JobBoardServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
