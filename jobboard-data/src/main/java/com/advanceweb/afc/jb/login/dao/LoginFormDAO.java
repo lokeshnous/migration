ea328065e8a9dadb0beb579435d0187ce8be6893
amitkumar <amitkumar@localhost>
2012-10-30 11:56:54 +0000

    updated login/dao/LoginFormDAO.java is added is new functionality

--- jobboard-data/src/main/java/com/advanceweb/afc/jb/login/dao/LoginFormDAO.java
+++ jobboard-data/src/main/java/com/advanceweb/afc/jb/login/dao/LoginFormDAO.java
@@ -1,6 +1,10 @@
 package com.advanceweb.afc.jb.login.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import com.advanceweb.afc.jb.common.LoginDTO;
+import com.advanceweb.afc.jb.data.entities.JpJobStat;
 
 /**
  * 
@@ -12,17 +16,39 @@ import com.advanceweb.afc.jb.common.LoginDTO;
 public interface LoginFormDAO {
 	/**
 	 * This method to validate the userId and roleID of logged in user
+	 * 
 	 * @param emailAddress
 	 * @param password
 	 * @return
 	 */
-	LoginDTO validateLoginFormValues(String emailAddress,
-			String password);
-	
+	LoginDTO validateLoginFormValues(String emailAddress, String password);
+
 	/**
 	 * This method to get the user details
+	 * 
 	 * @param emailAddress
 	 * @return
 	 */
 	LoginDTO getUserEmailDetails(String emailAddress);
+
+	/**
+	 * This method is to get all list of Dates
+	 * 
+	 * @param startFrom
+	 *            ,endFrom,facilityId
+	 * @return
+	 * @throws JobBoardServiceException
+	 */
+
+	List<JpJobStat> employerMetrics(Date startFrom, Date endFrom,
+			int selEmployerId);
+
+	/**
+	 * This method is to get active job Posting
+	 * 
+	 * @param facilityId
+	 * @return
+	 * @throws JobBoardServiceException
+	 */
+	int getactivejobposting(int facilityId);
 }
