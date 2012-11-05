98829e5c1cdf29d743340b73d15878daac709894
amitkumar <amitkumar@localhost>
2012-11-05 08:27:46 +0000

    updated source code: with respect to my recent search functionality.

---
+++ jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/MyRecentSearchesForm.java
@@ -0,0 +1,93 @@
+package com.advanceweb.afc.jb.jobseeker.web.controller;
+
+public class MyRecentSearchesForm {
+	
+	private String keywords;
+	private String rows;
+	private String start;
+	private String cityState;
+	private String radius;
+	private String searchtype;
+	private String saveSearchName;
+	private boolean autoload;
+	private String jobTitlePage;
+	private String employerPage;
+	private String locationPage;
+	private boolean browseBy;
+	
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getRows() {
+		return rows;
+	}
+	public void setRows(String rows) {
+		this.rows = rows;
+	}
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getCityState() {
+		return cityState;
+	}
+	public void setCityState(String cityState) {
+		this.cityState = cityState;
+	}
+	public String getRadius() {
+		return radius;
+	}
+	public void setRadius(String radius) {
+		this.radius = radius;
+	}
+	public String getSearchtype() {
+		return searchtype;
+	}
+	public void setSearchtype(String searchtype) {
+		this.searchtype = searchtype;
+	}
+	public String getSaveSearchName() {
+		return saveSearchName;
+	}
+	public void setSaveSearchName(String saveSearchName) {
+		this.saveSearchName = saveSearchName;
+	}
+	public boolean isAutoload() {
+		return autoload;
+	}
+	public void setAutoload(boolean autoload) {
+		this.autoload = autoload;
+	}
+	public String getJobTitlePage() {
+		return jobTitlePage;
+	}
+	public void setJobTitlePage(String jobTitlePage) {
+		this.jobTitlePage = jobTitlePage;
+	}
+	public String getEmployerPage() {
+		return employerPage;
+	}
+	public void setEmployerPage(String employerPage) {
+		this.employerPage = employerPage;
+	}
+	public String getLocationPage() {
+		return locationPage;
+	}
+	public void setLocationPage(String locationPage) {
+		this.locationPage = locationPage;
+	}
+	public boolean isBrowseBy() {
+		return browseBy;
+	}
+	public void setBrowseBy(boolean browseBy) {
+		this.browseBy = browseBy;
+	}
+	
+	
+
+}
