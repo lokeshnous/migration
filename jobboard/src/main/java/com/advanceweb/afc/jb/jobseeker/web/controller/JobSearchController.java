1c5db6616d696177aad319f3cb9f3affc76d481e
reeteshrn <reeteshrn@localhost>
2012-11-14 06:32:14 +0000

    Removed unused imports from this file

--- jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobSearchController.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/jobseeker/web/controller/JobSearchController.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
