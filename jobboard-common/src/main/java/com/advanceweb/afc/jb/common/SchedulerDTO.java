a93f4b7af9a78d3514420d6516255e81ea6a7d6e
anilm <anilm@localhost>
2012-11-15 12:31:26 +0000

    DTO is added to perform the schedular related tasks.

---
+++ jobboard-common/src/main/java/com/advanceweb/afc/jb/common/SchedulerDTO.java
@@ -0,0 +1,60 @@
+package com.advanceweb.afc.jb.common;
+
+/**
+ * This DTO will be used by scheduler to perform the scheduler tasks
+ * @author anilm
+ * @since Nov 15, 2012
+ */
+public class SchedulerDTO {
+
+	private int userId;
+	private String emailId;
+	private String firstName;
+	private String lastName;
+	private int jobId;
+	private String companyName;
+	private String expireDate;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getJobId() {
+		return jobId;
+	}
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(String expireDate) {
+		this.expireDate = expireDate;
+	}
+}
