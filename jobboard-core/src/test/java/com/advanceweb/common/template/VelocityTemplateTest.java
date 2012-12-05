9872ba9bb6aba312853b797d27e172669d425135
sukesh <sukesh@localhost>
2012-12-05 08:49:16 +0000

    Improved the Velocity template test to test the ad tag template that will automatically render all the variables to the vars section of the ad tag.

--- jobboard-core/src/test/java/com/advanceweb/common/template/VelocityTemplateTest.java
+++ jobboard-core/src/test/java/com/advanceweb/common/template/VelocityTemplateTest.java
@@ -1,5 +1,6 @@
 package com.advanceweb.common.template;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
@@ -11,6 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.advanceweb.jb.test.ServiceTestBase;
 
+/**
+ * Test case for the Velocity template. The test uses a template file similar to
+ * the one used for ad tags. This file covers pretty much what we need to use
+ * the template for.
+ * 
+ * @author sukeshnambiar
+ * 
+ */
 public class VelocityTemplateTest extends ServiceTestBase {
 	private static final Logger LOGGER = Logger
 			.getLogger(VelocityTemplateTest.class);
@@ -24,15 +33,67 @@ public class VelocityTemplateTest extends ServiceTestBase {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("auid", 297453);
 		params.put("url", "http://ox-d.advanceweb.com");
-		Map<String, Object> vars = new HashMap<String, Object>();
-		vars.put("topic", "nurse");
-		params.put("vars", vars);
-		String result = velocityTestTemplate.process(params);
+
+		String result;
+
+		// Check the template without var parameters
+		result = velocityTestTemplate.process(params);
 		LOGGER.debug(result);
-		assertTrue(String.format(ERROR_MSG, "topic", "nurse"),
-				result.indexOf("nurse") >= 0);
 		assertTrue(String.format(ERROR_MSG, "auid", "297453"),
 				result.indexOf("297453") >= 0);
-	}
+		assertFalse(
+				"A var section is added in the tag even though no variables are passed",
+				result.indexOf("vars") >= 0);
 
+		// Check with var parameter keyword
+		Map<String, Object> vars = new HashMap<String, Object>();
+		params.put("vars", vars);
+
+		vars.put("keyword", "nurse");
+		result = velocityTestTemplate.process(params);
+		LOGGER.debug(result);
+		assertTrue(String.format(ERROR_MSG, "auid", "297453"),
+				result.indexOf("297453") >= 0);
+		assertTrue(String.format(ERROR_MSG, "keyword", "nurse"),
+				result.indexOf("nurse") >= 0);
+
+		// Check with state variable
+		vars.put("state", "new york");
+
+		result = velocityTestTemplate.process(params);
+		LOGGER.debug(result);
+		assertTrue(String.format(ERROR_MSG, "auid", "297453"),
+				result.indexOf("297453") >= 0);
+		assertTrue(String.format(ERROR_MSG, "keyword", "nurse"),
+				result.indexOf("\"nurse\"") >= 0);
+		assertTrue(String.format(ERROR_MSG, "state", "new york"),
+				result.indexOf("\"state\":\"new york\"") >= 0);
+
+		// Check with state variable
+		vars.put("city", "Puerto Rico");
+		result = velocityTestTemplate.process(params);
+		LOGGER.debug(result);
+		assertTrue(String.format(ERROR_MSG, "auid", "297453"),
+				result.indexOf("297453") >= 0);
+		assertTrue(String.format(ERROR_MSG, "keyword", "nurse"),
+				result.indexOf("\"nurse\"") >= 0);
+		assertTrue(String.format(ERROR_MSG, "state", "new york"),
+				result.indexOf("\"state\":\"new york\"") >= 0);
+		assertTrue(String.format(ERROR_MSG, "state", "new york"),
+				result.indexOf("\"city\":\"Puerto Rico\"") >= 0);
+
+		// Remove the state variable
+		vars.remove("state");
+		result = velocityTestTemplate.process(params);
+		LOGGER.debug(result);
+		assertTrue(String.format(ERROR_MSG, "auid", "297453"),
+				result.indexOf("297453") >= 0);
+		assertTrue(String.format(ERROR_MSG, "keyword", "nurse"),
+				result.indexOf("\"nurse\"") >= 0);
+		assertFalse(
+				"State variable is added even though it is not specified in the variables",
+				result.indexOf("\"state\":\"new york\"") >= 0);
+		assertTrue(String.format(ERROR_MSG, "state", "new york"),
+				result.indexOf("\"city\":\"Puerto Rico\"") >= 0);
+	}
 }
