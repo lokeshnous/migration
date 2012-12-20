eb10553c7484d48837a5a97554eab19025a9deb1
princem <princem@localhost>
2012-12-20 08:25:05 +0000

    Updated the file as per the Advance Pass changes

--- jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/PreAuthenticationManager.java
+++ jobboard/src/main/java/com/advanceweb/afc/jb/login/web/controller/PreAuthenticationManager.java
@@ -17,6 +17,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.core.Authentication;
@@ -31,6 +32,8 @@ import com.advanceweb.afc.jb.security.DatabaseAuthenticationManager;
 import com.advanceweb.afc.jb.user.UserService;
 
 public class PreAuthenticationManager extends AbstractPreAuthenticatedProcessingFilter{
+	private static final Logger LOGGER = Logger
+			.getLogger(PreAuthenticationManager.class);
 	  private AuthenticationDetailsSource ads = new WebAuthenticationDetailsSource();
 	  @Autowired
 	  private DatabaseAuthenticationManager authenticationManager;
@@ -45,30 +48,30 @@ public class PreAuthenticationManager extends AbstractPreAuthenticatedProcessing
 	
 	@Override
 	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-		System.out.println("getPreAuthenticatedPrincipal=======>");
+		LOGGER.debug("getPreAuthenticatedPrincipal=======>");
 		 authentication = SecurityContextHolder.getContext().getAuthentication();
 		    HttpServletRequest req=(HttpServletRequest)request;
 		    if (authentication == null) {   
 		    	String userEmail="";
 		    	for(Cookie cookie:req.getCookies()){
 		    		//boolean value=cookie.getName().equals(".ASPXAUTH");
-		    		System.out.println("0 cookie.getName()=======>");
+		    		LOGGER.debug("0 cookie.getName()=======>");
 		    		//boolean value=false;
 //		    		cookie.getName().equals(".ASPXAUTH")
-					if(true){
-						System.out.println("1 .ASPXAUTH cookie is there=======>");
+					if(cookie.getName().equals(".ASPXAUTH")){
+						LOGGER.debug("1 .ASPXAUTH cookie is there=======>");
 						//String cookieValue=cookie.getValue();
-						String cookieValue="DD0D76549C2BC74A23D04236BE4AAFDF3B88D304F9EE7302E9F8FCE06D651B97DDEAE3E1A8CA0CED79C845A91656C1CE472F7C98D785AC20BE7091BE623CC790C6B079254BB0195F2B8E5C5C4D9B0C022C508F5F6979B320C1D8FC872723625D90456F51A5EEEF099B69F31C";
-//						String cookieValue=cookie.getValue();
-						System.out.println("2 cookieValue=======>"+cookieValue);
+//						String cookieValue="E349A4BE8158C56A72B47385C985CCE72D84498052D28130231E179CCD00CF9F10B475B05B70AC89DFDD07435B5863180224634DF8312E38E6477E74ECB3541B4560838235D41BAE9BF0D3DD9547C9A86FED6BEDF8FE1BFA0A45CEE09941C1839E02C4D67AE5BD9EFD4E47C1";
+						String cookieValue=cookie.getValue();
+						LOGGER.debug("2 cookieValue=======>"+cookieValue);
 						Date date =new Date();
 						SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH.mm.ss aa", Locale.US);
 						String postData = cookieValue + "^" + formatter.format(date) + "^" + "30" + "^" + "true";
 						
 						try {
 							//URLConnection connection = new URL("http://12.104.61.122:901/testinterpreter.ashx?").openConnection();
-							URL url = new URL("https://securedev.advanceweb.com/interpreter.ashx");
-							//URL url = new URL("http://12.104.61.122:901/testinterpreter.ashx");
+//							URL url = new URL("https://securedev.advanceweb.com/interpreter.ashx");
+							URL url = new URL("http://12.104.61.122:901/testinterpreter.ashx");
 							TrustManager[] trustAllCerts = new TrustManager[]{
 				                       new X509TrustManager() {
 				                           public java.security.cert.X509Certificate[] getAcceptedIssuers() {return null;}
@@ -96,26 +99,25 @@ public class PreAuthenticationManager extends AbstractPreAuthenticatedProcessing
 							//userEmail="fdnyrk@sbcglobal.net";
 							/*while ((userEmail = in.readLine()) != null)*/
 							userEmail=in.readLine();
-								System.out.println("3 userEmail=======>"+userEmail);
+							LOGGER.debug("3 userEmail=======>"+userEmail);
 							/*while (in.readLine() != null)
 							{
 								userEmail = in.readLine();
 								System.out.println("email in loop"+userEmail);
 							}*/
 							/*while ((userEmail = in.readLine()) != null)*/
-								System.out.println("3 userEmail=======>"+userEmail);
+							LOGGER.debug("3 userEmail=======>"+userEmail);
 								in.close();
 						} catch (Exception e) {
-							System.out.println("4 exception===>"+e);
-							e.printStackTrace();
-							// TODO: handle exception
+							LOGGER.debug("4 exception===>"+e);
+						
 						}	
 						break;
 					}
 				}
 		    	
 		        if(userEmail!=null && userEmail.contains("@")){
-		        	System.out.println("user email is  "+userEmail);
+		        	LOGGER.debug("user email is  "+userEmail);
 		        	UserDTO jbuser = userService.getUser(userEmail);
 		        	if(jbuser!=null){
 		        	user = userService.getAdvancePassUser(userEmail);
@@ -148,7 +150,7 @@ public class PreAuthenticationManager extends AbstractPreAuthenticatedProcessing
 
 	@Override
 	protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
-		System.out.println("getPreAuthenticatedCredentials=======>");
+		LOGGER.debug("getPreAuthenticatedCredentials=======>");
 		//return authentication != null ?authentication.getCredentials():null;
 		//return auth != null ?auth.getPassword():null;
 		return user != null ?user.getPassword():null;
@@ -156,10 +158,10 @@ public class PreAuthenticationManager extends AbstractPreAuthenticatedProcessing
 
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) {
 		 try {
-			 System.out.println("in successfulAuthentication of pre auth");
+			 LOGGER.debug("in successfulAuthentication of pre auth");
 			loginManager.onAuthenticationSuccess(request, response, authResult);
 		} catch (Exception e) {
-			System.out.println("exception in onAuthenticationSuccess");
+			LOGGER.debug("exception in onAuthenticationSuccess");
 			e.printStackTrace();
 		}
 	    }
